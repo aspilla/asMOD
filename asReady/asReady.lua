99c8a374b7ed78a74c8d9b1327ab2a58255c3abd
aspilla <aspilla@gmail.com>
2022-12-11 22:24:48 +0900

    오류수정정

--- asReady/asReady.lua
+++ asReady/asReady.lua
@@ -15,18 +15,14 @@ local AREADY_UpdateRate = 0.2 -- Refresh 시간 초
 
 local trackedPartySpells={
     [6552]  = 15, -- Pummel
-    [386071] = 90, -- Disrupting Shout
     [1766]  = 15, -- Kick
     [183752] = 15, --Disrupt
 	[116705] = 15, --Spear Hand Strike
-    [47482] = 30, -- Leap
     [47528]   = 15, --Mind Freeze
     [187707] = 15, -- Muzzle
     [147362]  = 24, -- Counter Shot
-    [351338]  = 40, -- Quell
+    [351338]  = 20, -- Quell
 	[106839]  = 15, -- "Skull Bash
-    [78675]   = 60, -- Solar Beam
-    [212619]  = 30, -- Call Felhunter
     [119898]  = 24, -- Command Demon
 	[15487] = 45, -- Silence
 	[31935] = 15, -- Avenger's Shield
@@ -35,29 +31,6 @@ local trackedPartySpells={
 	[2139] = 24, -- Counterspell
 
 }
---[[
-	OmniCD 로 부터
-		["name"]="Pummel",["duration"]=15,["spellID"]=6552, },
-		["name"]="Disrupting Shout",["duration"]=90,["spellID"]=386071, },
-		["name"]="Kick",["duration"]=15,["spellID"]=1766, },
-		["name"]="Disrupt",["duration"]=15,["spellID"]=183752, },
-		["name"]="Spear Hand Strike",["duration"]=15,["spellID"]=116705, },
-		["duration"]=30,["name"]="Leap",["spellID"]=47482, },
-		["name"]="Mind Freeze",["duration"]=15,["spellID"]=47528, },
-		["name"]="Muzzle",["ID"]=25,["duration"]=15,["icon"]=1376045,["spellID"]=187707, },
-			["name"]="Counter Shot",["ID"]=100,["duration"]=24,["icon"]=249170,["spellID"]=147362, },
-			["name"]="Quell",["ID"]=98,["duration"]=40,["icon"]=4622469,["spellID"]=351338, },
-		["name"]="Skull Bash",["ID"]=148,["duration"]=15,["icon"]=236946,["spellID"]=106839, },
-			["name"]="Solar Beam",["ID"]=202,["duration"]=60,["icon"]=252188,["spellID"]=78675, },
-		["duration"]=30,["name"]="Call Felhunter",["icon"]=136174,["spellID"]=212619, },
-	["duration"]=24,["name"]="Command Demon",["icon"]=236292,["spellID"]=119898, },
-	["name"]="Silence",["ID"]=99,["duration"]=45,["icon"]=458230,["spellID"]=15487, },
-		["name"]="Avenger's Shield",["ID"]=33,["duration"]=15,["icon"]=135874,["spellID"]=31935, },
-		["name"]="Rebuke",["ID"]=135,["duration"]=15,["icon"]=523893,["spellID"]=96231, },
-		["name"]="Wind Shear",["ID"]=131,["duration"]=12,["icon"]=136018,["spellID"]=57994, },
-			["duration"]=24,["name"]="Counterspell",["icon"]=135856,["spellID"]=2139, },
-]]	
-
 
 -----------------설정 끝 ------------------------
 
@@ -241,49 +214,15 @@ local function AREADY_OnEvent(self, event, arg1, arg2, arg3, arg4, arg5)
 			if trackedPartySpells[spellid] then
 				
 				local cool = trackedPartySpells[spellid];
-	
-				if UnitIsUnit("playerpet", unit) then
-        	    	unit="player"
-            	else
-                	for i=1,GetNumGroupMembers()-1 do
-                		if UnitIsUnit("partypet"..i, unit) then
-                      		unit="party"..i;
-	                        break
-			        	end
-	                end
-    	        end
-                
+        
                 -- Create a reference to pass to several functions
         	    if UnitIsUnit("player", unit) then
-					local bfind = false
-					for i, v in pairs(partycool) do 
-						if v[1] == 0 and   v[2] == spellid then
-							bfind = true;
-							partycool[i] = {0, spellid, time, cool};
-							break;
-						end					
-					end
-
-					if not bfind and #partycool <= AREDDY_Max then
-	            	    table.insert(partycool, {0, spellid, time, cool});
-					end
+					partycool[0] = {0, spellid, time, cool};
 				else
     	        	for k=1,GetNumGroupMembers()-1 do 
 
 						if UnitIsUnit("party"..k, unit) then
-
-							local bfind = false
-							for i, v in pairs(partycool) do 
-								if v[1] == k and   v[2] == spellid then
-									bfind = true;
-									partycool[i] = {k, spellid, time, cool};
-									break;
-								end					
-							end
-		
-							if not bfind and #partycool <= AREDDY_Max then
-								table.insert(partycool, {k, spellid, time, cool});
-							end
+							partycool[k] = {k, spellid, time, cool};
 						end
 					end
 	            end
