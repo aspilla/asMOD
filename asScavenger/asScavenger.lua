7288978c651e3fd726d6f905f43f08fdfac64cf3
aspilla <aspilla@gmail.com>
2022-12-18 17:43:01 +0900

    오류 가능성 수정

--- asScavenger/asScavenger.lua
+++ asScavenger/asScavenger.lua
@@ -1,16 +1,21 @@
 local onEvent = function(self)
 	local c = C_Container;
 	
-	for b=0,4 do 
-		for s=1,c.GetContainerNumSlots(b) do 
-			local n = c.GetContainerItemLink(b,s);
-
-			if n and string.find(n,"ff9d9d9d") then 
-				print(n,"판매"); 
-				c.UseContainerItem(b,s);
-			end
-		end
-	end 
+	for bag = 0, 4 do
+    	if c.GetContainerNumSlots(bag) > 0 then
+			for slot = 0, c.GetContainerNumSlots(bag) do
+				local link = c.GetContainerItemLink(bag, slot)
+			
+				if link then
+					local itemName, itemLink, itemRarity = GetItemInfo(link)
+				
+					if itemRarity == 0 then
+						c.UseContainerItem(bag, slot)
+					end
+				end
+			  end
+	    end
+	end
 end
 
 local asScavenger = CreateFrame("Frame", "ASV_main", UIParent);
