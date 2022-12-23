b068ac7b290a9a523f210b775df0a3fa6a17bf27
aspilla <aspilla@gmail.com>
2022-12-23 17:51:42 +0900

    Release

--- asTargetCastBar/asTargetCastBar.lua
+++ asTargetCastBar/asTargetCastBar.lua
@@ -1,15 +1,17 @@
 ﻿-----------------설정 ------------------------
 local ATCB_WIDTH = 150
-local ATCB_HEIGHT = 15
+local ATCB_HEIGHT = 17
 local ATCB_X = 0;
-local ATCB_Y = 0;
-local ATCB_ALPHA = 0.8;
-local ATCB_NAME_SIZE = 14;
-local ATCB_TIME_SIZE = 10;
+local ATCB_Y = -50;
+local ATCB_ALPHA = 0.8;											--투명도 80%
+local ATCB_NAME_SIZE = ATCB_HEIGHT * 0.7;						--Spell 명 Font Size, 높이의 70%
+local ATCB_TIME_SIZE = ATCB_HEIGHT * 0.5;						--Spell 시전시간 Font Size, 높이의 50%
+local ATCB_NOT_INTERRUPTIBLE_COLOR = {0.8, 0.8, 0.8}; 			--차단 불가시 (내가 아닐때) 색상 (r, g, b)
+local ATCB_NOT_INTERRUPTIBLE_COLOR_TARGET = {0.8, 0.5, 0.5}; 	--차단 불가시 (내가 타겟일때) 색상 (r, g, b)
+local ATCB_INTERRUPTIBLE_COLOR = {0, 0.9, 0}; 					--차단 가능(내가 타겟이 아닐때)시 색상 (r, g, b)
+local ATCB_INTERRUPTIBLE_COLOR_TARGET = {1, 0.5, 0}; 			--차단 가능(내가 타겟일 때)시 색상 (r, g, b)
 
-
-
-
+-----------------설정 끝끝------------------------
 local ATCB = CreateFrame("FRAME", nil, UIParent)
 ATCB:SetPoint("BOTTOM",UIParent,"BOTTOM", 0, 0)
 ATCB:SetWidth(0)
@@ -22,7 +24,7 @@ ATCB.castbar:GetStatusBarTexture():SetHorizTile(false)
 ATCB.castbar:SetMinMaxValues(0, 100)
 ATCB.castbar:SetValue(100)
 ATCB.castbar:SetHeight(ATCB_HEIGHT)
-ATCB.castbar:SetWidth(ATCB_WIDTH)
+ATCB.castbar:SetWidth(ATCB_WIDTH - ATCB_HEIGHT/2)
 ATCB.castbar:SetStatusBarColor(1, 0.9, 0.9);
 ATCB.castbar:SetAlpha(ATCB_ALPHA);
 
@@ -37,13 +39,13 @@ ATCB.castbar.bg:Show();
 
 ATCB.castbar.name = ATCB.castbar:CreateFontString( nil , "OVERLAY");
 ATCB.castbar.name:SetFont(STANDARD_TEXT_FONT, ATCB_NAME_SIZE);
-ATCB.castbar.name:SetPoint("CENTER", ATCB.castbar, "CENTER", 0, 0);
+ATCB.castbar.name:SetPoint("LEFT", ATCB.castbar, "LEFT", 3, 0);
 
 ATCB.castbar.time = ATCB.castbar:CreateFontString( nil , "OVERLAY");
 ATCB.castbar.time:SetFont(STANDARD_TEXT_FONT, ATCB_TIME_SIZE);
 ATCB.castbar.time:SetPoint("RIGHT", ATCB.castbar, "RIGHT", -3, 0);
 
-ATCB.castbar:SetPoint("CENTER",UIParent,"CENTER", ATCB_X, ATCB_Y)
+ATCB.castbar:SetPoint("CENTER",UIParent,"CENTER", ATCB_X + ATCB_HEIGHT/2, ATCB_Y)
 ATCB.castbar:Hide();
 
 ATCB.button = CreateFrame("Button", "ATCBIcon", ATCB.castbar, "ATCBFrameTemplate");
@@ -81,6 +83,7 @@ local function ATCB_OnEvent(self, event, ...)
 			ATCB:RegisterUnitEvent("UNIT_SPELLCAST_CHANNEL_START", "target");
 			ATCB:RegisterUnitEvent("UNIT_SPELLCAST_CHANNEL_STOP", "target");
 			ATCB:RegisterUnitEvent("UNIT_SPELLCAST_STOP", "target");
+			ATCB:RegisterUnitEvent("UNIT_TARGET", "target");
 		else
 			ATCB:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED");
 			ATCB:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTED");
@@ -89,6 +92,7 @@ local function ATCB_OnEvent(self, event, ...)
 			ATCB:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_START");
 			ATCB:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_STOP");
 			ATCB:UnregisterEvent("UNIT_SPELLCAST_STOP");
+			ATCB:UnregisterEvent("UNIT_TARGET");
 		end	
 	end
 
@@ -112,11 +116,26 @@ local function ATCB_OnEvent(self, event, ...)
 			castBar:SetMinMaxValues(0, self.duration)
 			castBar:SetValue(current - self.start);
 
-			if notInterruptible then
-				castBar:SetStatusBarColor(0.1, 0.1, 0.1);
+			local color = {};
+
+			if UnitIsUnit("targettarget", "player") then
+				if notInterruptible then
+					color = ATCB_NOT_INTERRUPTIBLE_COLOR_TARGET;
+				else
+					color = ATCB_INTERRUPTIBLE_COLOR_TARGET;
+				end
+
 			else
-				castBar:SetStatusBarColor(0, 0.9, 0);
+				if notInterruptible then
+					color = ATCB_NOT_INTERRUPTIBLE_COLOR;
+				else
+					color = ATCB_INTERRUPTIBLE_COLOR;
+				end
+
 			end
+
+			castBar:SetStatusBarColor(color[1], color[2], color[3]);
+						
 			text:SetText(name);
 			time:SetText(format("%.1f/%.1f", max((current - self.start), 0), max(self.duration, 0)));
 			
