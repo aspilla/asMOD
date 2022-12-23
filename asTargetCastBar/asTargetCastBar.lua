905c92a10cdd3c8ed7027cc28e751438773335d6
aspilla <aspilla@gmail.com>
2022-12-23 13:09:14 +0900

    1.0

--- asTargetCastBar/asTargetCastBar.lua
+++ asTargetCastBar/asTargetCastBar.lua
@@ -1,59 +1,154 @@
 ﻿-----------------설정 ------------------------
-local AGCDB_WIDTH = 65
-local AGCDB_HEIGHT = 5
-local AGCDB_X = 0;
-local AGCDB_Y = -76;
+local ATCB_WIDTH = 150
+local ATCB_HEIGHT = 15
+local ATCB_X = 0;
+local ATCB_Y = 0;
+local ATCB_ALPHA = 0.8;
+local ATCB_NAME_SIZE = 14;
+local ATCB_TIME_SIZE = 10;
 
 
 
 
-local AGCDB = CreateFrame("FRAME", nil, UIParent)
-AGCDB:SetPoint("BOTTOM",UIParent,"BOTTOM", 0, 0)
-AGCDB:SetWidth(0)
-AGCDB:SetHeight(0)
-AGCDB:Show();
+local ATCB = CreateFrame("FRAME", nil, UIParent)
+ATCB:SetPoint("BOTTOM",UIParent,"BOTTOM", 0, 0)
+ATCB:SetWidth(0)
+ATCB:SetHeight(0)
+ATCB:Show();
 
+ATCB.castbar = CreateFrame("StatusBar", nil, UIParent)
+ATCB.castbar:SetStatusBarTexture("Interface\\addons\\asTargetCastBar\\UI-StatusBar.blp", "BORDER")
+ATCB.castbar:GetStatusBarTexture():SetHorizTile(false)
+ATCB.castbar:SetMinMaxValues(0, 100)
+ATCB.castbar:SetValue(100)
+ATCB.castbar:SetHeight(ATCB_HEIGHT)
+ATCB.castbar:SetWidth(ATCB_WIDTH)
+ATCB.castbar:SetStatusBarColor(1, 0.9, 0.9);
+ATCB.castbar:SetAlpha(ATCB_ALPHA);
 
-AGCDB.gcdbar = CreateFrame("StatusBar", nil, UIParent)
-AGCDB.gcdbar:SetStatusBarTexture("Interface\\addons\\asGCDBar\\UI-StatusBar.blp", "BORDER")
-AGCDB.gcdbar:GetStatusBarTexture():SetHorizTile(false)
-AGCDB.gcdbar:SetMinMaxValues(0, 100)
-AGCDB.gcdbar:SetValue(100)
-AGCDB.gcdbar:SetHeight(AGCDB_HEIGHT)
-AGCDB.gcdbar:SetWidth(AGCDB_WIDTH)
-AGCDB.gcdbar:SetStatusBarColor(1, 0.9, 0.9);
+ATCB.castbar.bg = ATCB.castbar:CreateTexture(nil, "BACKGROUND")
+ATCB.castbar.bg:SetPoint("TOPLEFT", ATCB.castbar, "TOPLEFT", -1, 1)
+ATCB.castbar.bg:SetPoint("BOTTOMRIGHT", ATCB.castbar, "BOTTOMRIGHT", 1, -1)
 
-AGCDB.gcdbar.bg = AGCDB.gcdbar:CreateTexture(nil, "BACKGROUND")
-AGCDB.gcdbar.bg:SetPoint("TOPLEFT", AGCDB.gcdbar, "TOPLEFT", -1, 1)
-AGCDB.gcdbar.bg:SetPoint("BOTTOMRIGHT", AGCDB.gcdbar, "BOTTOMRIGHT", 1, -1)
+ATCB.castbar.bg:SetTexture("Interface\\Addons\\asTargetCastBar\\border.tga")
+ATCB.castbar.bg:SetTexCoord(0.1,0.1, 0.1,0.1, 0.1,0.1, 0.1,0.1)	
+ATCB.castbar.bg:SetVertexColor(0, 0, 0, 0.8);
+ATCB.castbar.bg:Show();
 
-AGCDB.gcdbar.bg:SetTexture("Interface\\Addons\\asGCDBar\\border.tga")
-AGCDB.gcdbar.bg:SetTexCoord(0.1,0.1, 0.1,0.1, 0.1,0.1, 0.1,0.1)	
-AGCDB.gcdbar.bg:SetVertexColor(0, 0, 0, 0.8);
+ATCB.castbar.name = ATCB.castbar:CreateFontString( nil , "OVERLAY");
+ATCB.castbar.name:SetFont(STANDARD_TEXT_FONT, ATCB_NAME_SIZE);
+ATCB.castbar.name:SetPoint("CENTER", ATCB.castbar, "CENTER", 0, 0);
 
-AGCDB.gcdbar:SetPoint("CENTER",UIParent,"CENTER", AGCDB_X, AGCDB_Y)
-AGCDB.gcdbar:Show();
+ATCB.castbar.time = ATCB.castbar:CreateFontString( nil , "OVERLAY");
+ATCB.castbar.time:SetFont(STANDARD_TEXT_FONT, ATCB_TIME_SIZE);
+ATCB.castbar.time:SetPoint("RIGHT", ATCB.castbar, "RIGHT", -3, 0);
+
+ATCB.castbar:SetPoint("CENTER",UIParent,"CENTER", ATCB_X, ATCB_Y)
+ATCB.castbar:Hide();
+
+ATCB.button = CreateFrame("Button", "ATCBIcon", ATCB.castbar, "ATCBFrameTemplate");
+ATCB.button:SetPoint("RIGHT", ATCB.castbar,"LEFT", -2, 0)
+ATCB.button:SetWidth(ATCB_HEIGHT * 1.2);
+ATCB.button:SetHeight(ATCB_HEIGHT);
+ATCB.button:SetScale(1);
+ATCB.button:SetAlpha(1);
+ATCB.button:EnableMouse(false);
+ATCB.button.icon:SetTexCoord(.08, .92, .08, .92);
+ATCB.button.border:SetTexCoord(0.08,0.08, 0.08,0.92, 0.92,0.08, 0.92,0.92);
+ATCB.button.border:SetVertexColor(0, 0, 0);
+ATCB.button.border:Show();
+ATCB.button:Show();
+
+ATCB.start = 0;
+ATCB.duration = 0;
+
 
 LoadAddOn("asMOD");
 
 if asMOD_setupFrame then
-    asMOD_setupFrame (AGCDB.gcdbar, "asGCDBar");
+    asMOD_setupFrame (ATCB.castbar, "asTargetCastBar");
 end
-local function AGCDB_OnUpdate()
 
-	local start, gcd  = GetSpellCooldown(61304);
-	local current = GetTime();
+local function ATCB_OnEvent(self, event, ...)
 
-	if gcd > 0 then
-		AGCDB.gcdbar:SetMinMaxValues(0, gcd * 1000)
-		AGCDB.gcdbar:SetValue((current - start) * 1000);			
-		AGCDB.gcdbar.start = start;
-		AGCDB.gcdbar.duration = gcd;
-		AGCDB.gcdbar:Show();
-	else
-		AGCDB.gcdbar:SetValue(0);
-		AGCDB.gcdbar:Hide();
+	if event == "PLAYER_TARGET_CHANGED" or event == "PLAYER_ENTERING_WORLD" then
+		
+		if UnitExists("target") then
+			ATCB:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "target");
+			ATCB:RegisterUnitEvent("UNIT_SPELLCAST_INTERRUPTED", "target");
+			ATCB:RegisterUnitEvent("UNIT_SPELLCAST_START", "target");
+			ATCB:RegisterUnitEvent("UNIT_SPELLCAST_DELAYED", "target");
+			ATCB:RegisterUnitEvent("UNIT_SPELLCAST_CHANNEL_START", "target");
+			ATCB:RegisterUnitEvent("UNIT_SPELLCAST_CHANNEL_STOP", "target");
+			ATCB:RegisterUnitEvent("UNIT_SPELLCAST_STOP", "target");
+		else
+			ATCB:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED");
+			ATCB:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTED");
+			ATCB:UnregisterEvent("UNIT_SPELLCAST_START");
+			ATCB:UnregisterEvent("UNIT_SPELLCAST_DELAYED");
+			ATCB:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_START");
+			ATCB:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_STOP");
+			ATCB:UnregisterEvent("UNIT_SPELLCAST_STOP");
+		end	
 	end
+
+		local name,  text, texture, start, endTime, isTradeSkill, castID, notInterruptible, spellid = UnitCastingInfo("target");
+
+		if not name then
+			name,  text, texture, start, endTime, isTradeSkill, notInterruptible, spellid = UnitChannelInfo("target");
+		end
+		
+		local frameIcon = self.button.icon; 
+		local castBar = self.castbar;
+		local text  = self.castbar.name;
+		local time = self.castbar.time;
+		
+		if name then
+			local current = GetTime();
+			frameIcon:SetTexture(texture);
+
+			self.start = start / 1000;
+			self.duration = (endTime - start) /1000;
+			castBar:SetMinMaxValues(0, self.duration)
+			castBar:SetValue(current - self.start);
+
+			if notInterruptible then
+				castBar:SetStatusBarColor(0.1, 0.1, 0.1);
+			else
+				castBar:SetStatusBarColor(0, 0.9, 0);
+			end
+			text:SetText(name);
+			time:SetText(format("%.1f/%.1f", max((current - self.start), 0), max(self.duration, 0)));
+			
+			frameIcon:Show();			
+			castBar:Show();
+					
+		else
+			castBar:SetValue(0);
+			frameIcon:Hide();
+			castBar:Hide();
+			self.start = 0;
+		end
 end
 
-C_Timer.NewTicker(0.1, AGCDB_OnUpdate);
+
+local function ATCB_OnUpdate()
+
+	local start = ATCB.start;
+
+	if start > 0 then
+		local castBar = ATCB.castbar;
+		local duration = ATCB.duration;
+		local current = GetTime();
+		local time = ATCB.castbar.time;
+		castBar:SetValue((current - start));
+		time:SetText(format("%.1f/%.1f", max((current - start), 0), max(duration, 0)));
+	end
+	
+end
+
+ATCB:SetScript("OnEvent", ATCB_OnEvent)
+ATCB:RegisterEvent("PLAYER_TARGET_CHANGED");
+ATCB:RegisterEvent("PLAYER_ENTERING_WORLD");
+
+C_Timer.NewTicker(0.1, ATCB_OnUpdate);
