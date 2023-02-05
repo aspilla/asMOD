0365eee5b0c9c89452c9eca41aaf410506733f7c
aspilla <aspilla@gmail.com>
2023-02-06 01:25:37 +0900

    코드 정리

--- asDotFilter/asDotFilter.lua
+++ asDotFilter/asDotFilter.lua
@@ -1,23 +1,11 @@
-﻿local ADotF = nil;
-local ADotF_PLAYER_DEBUFF;
-local ADotF_TARGET_DEBUFF;
-local ADotF_DeBuffList = {}
-
-local ADotF_SIZE = 40;
-local ADotF_DEBUFF_X = 0;
-local ADotF_DEBUFF_Y = 10;
+﻿local ADotF_SIZE = 30;
 local ADotF_MAX_DEBUFF_SHOW = 5;
 local ADotF_ALPHA = 0.9;
 local ADotF_CooldownFontSize = 12;
-local ADotF_CountFontSize = 12;			
-
-
-ADotF_ShowList = nil;
-ADotF_NameList = {};
-
+local ADotF_CountFontSize = 12;	
 
 ADotF_ShowList_WARRIOR_1 = {
-	{"난도질" , 15 * 0.3},	
+	{"분쇄" , 15 * 0.3},	
 }
 
 ADotF_ShowList_WARRIOR_2 = {
@@ -25,9 +13,6 @@ ADotF_ShowList_WARRIOR_2 = {
 
 ADotF_ShowList_WARRIOR_3 = {
 }
-
-
-
 
 ADotF_ShowList_ROGUE_1 = {
 	{"파열" , 24 * 0.3},	
@@ -38,7 +23,7 @@ ADotF_ShowList_ROGUE_2 = {
 }
 
 ADotF_ShowList_ROGUE_3 = {
-	
+	{"파열" , 24 * 0.3},	
 }
 
 ADotF_ShowList_WARLOCK_1 = {
@@ -61,6 +46,8 @@ ADotF_ShowList_PRIEST_1 = {
 	{"어둠의 권능: 고통" , 16 * 0.3},
 }
 
+ADotF_ShowList_PRIEST_2 = {
+}
 
 ADotF_ShowList_PRIEST_3 = {
 	{"어둠의 권능: 고통" , 16 * 0.3},	
@@ -73,9 +60,13 @@ ADotF_ShowList_SHAMAN_1 = {
 ADotF_ShowList_SHAMAN_2 = {
 }
 
+ADotF_ShowList_SHAMAN_3 = {
+}
+
 ADotF_ShowList_DRUID_1 = {
 	{"달빛섬광" , 22 * 0.3},	
-	{"태양섬광" , 18 * 0.3},	
+	{"태양섬광" , 13.5 * 0.3},	
+	{"항성의 섬광" , 18 * 0.3},	
 }
 
 
@@ -84,20 +75,34 @@ ADotF_ShowList_DRUID_2 = {
 	{"도려내기" , 24 * 0.3},	
 }
 
+ADotF_ShowList_DRUID_3 = {
+	{"달빛섬광" , 22 * 0.3},		
+}
+
+ADotF_ShowList_DRUID_4 = {
+	{"달빛섬광" , 22 * 0.3},		
+}
+
 ADotF_ShowList_DEATHKNIGHT_1 = {
-	
+	{"서리 열병", 0},
 }
 
 ADotF_ShowList_DEATHKNIGHT_2 = {
-	
+	{"피의 역병", 0},
 }
 
 ADotF_ShowList_DEATHKNIGHT_3 = {
 	{"악성 역병", 24 * 0.3},
+	{"고름 상처", 0},
+}
+
+ADotF_ShowList_HUNTER_1 = {
+	{"날카로운 사격", 2},
+	{"독사 쐐기", 12 * 0.3},
 }
 
 ADotF_ShowList_HUNTER_2 = {
-
+	{"독사 쐐기", 12 * 0.3},
 }
 
 ADotF_ShowList_HUNTER_3 = {
@@ -105,14 +110,8 @@ ADotF_ShowList_HUNTER_3 = {
 }
 
 
-
-
-local Prev_ExTime = {};
-
-
 --설정 표시할 Unit
 local ADotF_UnitList = {
-	"target", 		-- 대상 표시 안하길 원하면 이 줄 삭제
 	"focus",			-- 주시대상 표시 안하길 원하면 이 줄 삭제
 	"boss1",
 	"boss2",
@@ -120,7 +119,12 @@ local ADotF_UnitList = {
 	"boss4",
 	"boss5",
 }
-local prev_info = {};
+
+--설정 끝
+
+local ADotF = nil;
+ADotF_ShowList = nil;
+ADotF_NameList = {};
 
 
 --Overlay stuff
@@ -196,14 +200,14 @@ function ADotF_ActionBarOverlayGlowAnimInMixin:OnPlay()
 	local frame = self:GetParent();
 	local frameWidth, frameHeight = frame:GetSize();
 	frame.spark:SetSize(frameWidth, frameHeight);
-	frame.spark:SetAlpha(0.3);
-	frame.innerGlow:SetSize(frameWidth / 2, frameHeight / 2);
-	frame.innerGlow:SetAlpha(1.0);
-	frame.innerGlowOver:SetAlpha(1.0);
-	frame.outerGlow:SetSize(frameWidth * 2, frameHeight * 2);
-	frame.outerGlow:SetAlpha(1.0);
-	frame.outerGlowOver:SetAlpha(1.0);
-	frame.ants:SetSize(frameWidth * 0.85, frameHeight * 0.85)
+	frame.spark:SetAlpha(0);
+	frame.innerGlow:SetSize(frameWidth, frameHeight);
+	frame.innerGlow:SetAlpha(1);
+	frame.innerGlowOver:SetAlpha(1);
+	frame.outerGlow:SetSize(frameWidth, frameHeight);
+	frame.outerGlow:SetAlpha(1);
+	frame.outerGlowOver:SetAlpha(1);
+	frame.ants:SetSize(frameWidth * 0.8, frameHeight * 0.8)
 	frame.ants:SetAlpha(0);
 	frame:Show();
 end
@@ -237,34 +241,37 @@ local function ADotF_UnitDebuff_Name(unit, buff, filter)
 	local name, icon, count, debuffType, duration, expirationTime, caster, isStealable, shouldConsolidate, spellId, canApplyAura, isBossDebuff, value1, value2, value3;
 	local i = 1;
 	repeat
-
 		name, icon, count, debuffType, duration, expirationTime, caster, isStealable, shouldConsolidate, spellId, canApplyAura, isBossDebuff, value1, value2, value3 = UnitDebuff(unit, i, filter);
 
 		if name == buff then
 			return 	name, icon, count, debuffType, duration, expirationTime, caster, isStealable, shouldConsolidate, spellId, canApplyAura, isBossDebuff, value1, value2, value3;
 		end
-
-
 		i = i + 1;
-
-
 	until (name == nil)
 
 	return name, icon, count, debuffType, duration, expirationTime, caster, isStealable, shouldConsolidate, spellId, canApplyAura, isBossDebuff, value1, value2, value3;
 
 end
 
-local function ADotF_UpdateDebuffAnchor(debuffName, index, anchorIndex, size, offsetX, right, parent, powerbar)
+local function asCooldownFrame_Clear(self)
+	self:Clear();
+end
 
-	local buff = _G[debuffName..index];
+local function asCooldownFrame_Set(self, start, duration, enable, forceShowDrawEdge, modRate)
+	if enable and enable ~= 0 and start > 0 and duration > 0 then
+		self:SetDrawEdge(forceShowDrawEdge);
+		self:SetCooldown(start, duration, modRate);
+	else
+		asCooldownFrame_Clear(self);
+	end
+end
+
+local function ADotF_UpdateDebuffAnchor(frames, index, size, offsetX, right, parent, isboss)
+
+	local buff = frames[index];
 	local point1 = "LEFT";
 	local point2 = "RIGHT";
 	local point3 = "RIGHT";
-
-	if powerbar and (powerbar:IsShown())then
-		parent = powerbar;	
-	end
-
 
 	if (right == false) then
 		point1 = "RIGHT";
@@ -272,40 +279,35 @@ local function ADotF_UpdateDebuffAnchor(debuffName, index, anchorIndex, size, of
 		point3 = "LEFT";
 		offsetX = -offsetX;
 	end
-	
-	if ( index == 1 ) then
-		buff:SetPoint(point1, parent, point2, ADotF_DEBUFF_X, ADotF_DEBUFF_Y);
-	else
 
-		buff:SetPoint(point1, _G[debuffName..anchorIndex], point3, offsetX, 0);
+	if ( index == 1 ) then
+		if isboss then
+			buff:SetPoint(point1, parent, point2, -60, 0);
+		else
+			buff:SetPoint(point1, parent, point2, 0, 0);
+		end
+	else
+		buff:SetPoint(point1, frames[index-1], point3, offsetX, 0);
 	end
 
 	-- Resize
 	buff:SetWidth(size);
-	buff:SetHeight(size);
-	local debuffFrame =_G[debuffName..index.."Border"];
-	debuffFrame:SetWidth(size+2);
-	debuffFrame:SetHeight(size+2);
+	buff:SetHeight(size * 0.8);
 end
 
 
 local function ADotF_UpdateDebuff(unit)
 
-	local selfName;
 	local numDebuffs = 1;
-	local frame, frameName;
+	local frame;
 	local frameIcon, frameCount, frameCooldown;
-	local name, rank, icon, count, debuffType, duration, expirationTime, caster, isStealable, shouldConsolidate, spellId ;
+	local icon, count, debuffType, duration, expirationTime, caster;
 	local color;
 	local frameBorder;
-	local maxIdx;
 	local parent;
-	local frametype;
-	local filter = nil;
-	local find = false
-	local powerbar;
-
-
+	local find = false;
+	local isboss = true;
+	
 	for i = 1, #ADotF_UnitList do
 		if unit == ADotF_UnitList[i] then
 			find = true;
@@ -317,128 +319,92 @@ local function ADotF_UpdateDebuff(unit)
 		return;
 	end
 
-
-	if (unit == "target") then
-		selfName = "ADotF_TargetDEBUFF_";
-		maxIdx = MAX_TARGET_DEBUFFS;
-		parent = _G["TargetFrame"];
-		powerbar = _G["TargetFramePowerBarAlt"];
-	elseif (unit == "focus") then
-		selfName = "ADotF_FocusDEBUFF_";
-		maxIdx = MAX_TARGET_DEBUFFS;
-		parent = _G["FocusFrame"];
-	elseif (unit == "boss1") then
-		selfName = "ADotF_Boss1DEBUFF_";
-		maxIdx = MAX_TARGET_DEBUFFS;
-		parent = _G["Boss1TargetFrame"];
-		powerbar = _G["Boss1TargetFramePowerBarAlt"];
-	elseif (unit == "boss2") then
-		selfName = "ADotF_Boss2DEBUFF_";
-		maxIdx = MAX_TARGET_DEBUFFS;
-		parent = _G["Boss2TargetFrame"];
-		powerbar = _G["Boss2TargetFramePowerBarAlt"];
-	elseif (unit == "boss3") then
-		selfName = "ADotF_Boss3DEBUFF_";
-		maxIdx = MAX_TARGET_DEBUFFS;
-		parent = _G["Boss3TargetFrame"];
-		powerbar = _G["Boss3TargetFramePowerBarAlt"];
-	elseif (unit == "boss4") then
-		selfName = "ADotF_Boss4DEBUFF_";
-		maxIdx = MAX_TARGET_DEBUFFS;
-		parent = _G["Boss4TargetFrame"];
-		powerbar = _G["Boss4TargetFramePowerBarAlt"];
-	elseif (unit == "boss5") then
-		selfName = "ADotF_Boss5DEBUFF_";
-		maxIdx = MAX_TARGET_DEBUFFS;
-		parent = _G["Boss5TargetFrame"];
-		powerbar = _G["Boss5TargetFramePowerBarAlt"];
-	else
-		return;
-	end
-
-	frametype = selfName.."Button";
-	isMine = {};
-	
 	if not ADotF_ShowList then
 		return;
 	end
 
-	if not Prev_ExTime[unit] then
-		Prev_ExTime[unit] = {}
+	if not ADotF.units then
+		ADotF.units = {};		
 	end
 
-	maxIdx =  #ADotF_ShowList;
+	if not ADotF.units[unit] then
+		ADotF.units[unit] = {};		
+	end
 
+	if not ADotF.units[unit].frames then
+		ADotF.units[unit].frames = {};		
+	end
 
-	--print ("update");
+	local maxIdx =  #ADotF_ShowList;
 
-	--for i = 1, maxIdx do
-	for i = 1, #ADotF_ShowList do 
-		local skip = false;
-		local debuff;
-
-			
-		local filter;
-
-		name, icon, count, debuffType, duration, expirationTime, caster, isStealable, shouldConsolidate, spellId, canApplyAura, isBossDebuff, value1, value2, value3   = ADotF_UnitDebuff_Name(unit, ADotF_ShowList[i][1], "PLAYER");
-
-		local prev = prev_info[unit][numDebuffs]
-
-		if unit == "target" and ACI_Debuff_list and ACI_Debuff_list[name] then
-			icon = nil;
-		end
-
-		if icon and  caster == "player" or caster == "pet"  then
-			
-			local bAlert = false;
-				
-			if numDebuffs > ADotF_MAX_DEBUFF_SHOW then
-				break;
-			end
-
-			if ((expirationTime - GetTime()) <= ADotF_ShowList[i][2]) and duration > 0  then
-				bAlert = true;
-			end
-
-		
-			if not ( prev[1] == spellId and prev[2] == duration and prev[3] == expirationTime and prev[4] == bAlert )then
-
-
-				prev_info[unit][numDebuffs] = {spellId, duration, expirationTime, bAlert};
-
-				Prev_ExTime[unit][i] = expirationTime;
-
-				frameName = frametype..numDebuffs;
+	if UnitExists(unit) then
 	
-				frame = _G[frameName];
+		if (unit == "target") then
+			parent = _G["TargetFrame"];
+			isboss = false;
+		elseif (unit == "focus") then
+			parent = _G["FocusFrame"];
+			isboss = false;
+		elseif (unit == "boss1") then
+			parent = _G["Boss1TargetFrame"];
+		elseif (unit == "boss2") then
+			parent = _G["Boss2TargetFrame"];
+		elseif (unit == "boss3") then
+			parent = _G["Boss3TargetFrame"];
+		elseif (unit == "boss4") then
+			parent = _G["Boss4TargetFrame"];
+		elseif (unit == "boss5") then
+			parent = _G["Boss5TargetFrame"];
+		else
+			return;
+		end
+			
+		for i = 1, #ADotF_ShowList do 
+		
+			_, icon, count, debuffType, duration, expirationTime, caster = ADotF_UnitDebuff_Name(unit, ADotF_ShowList[i][1], "PLAYER");
+
+			if icon and  caster == "player" or caster == "pet"  then
+		
+				if numDebuffs > ADotF_MAX_DEBUFF_SHOW then
+					break;
+				end
+		
+				frame = ADotF.units[unit].frames[numDebuffs];
 
 				if ( not frame ) then
-					frame = CreateFrame("Button", frameName, parent, "asTargetDotFrameTemplate");
+					ADotF.units[unit].frames[numDebuffs] = CreateFrame("Button", nil, ADotF, "asTargetDotFrameTemplate");
+					frame = ADotF.units[unit].frames[numDebuffs];
 					frame:EnableMouse(false); 
-					for _,r in next,{_G[frameName.."Cooldown"]:GetRegions()}	do 
+					for _,r in next,{frame.cooldown:GetRegions()}	do 
 						if r:GetObjectType()=="FontString" then 
-							r:SetFont("Fonts\\2002.TTF",ADotF_CooldownFontSize,"OUTLINE")
-							break 
+							r:SetFont("Fonts\\2002.TTF",ADotF_CooldownFontSize,"OUTLINE");
+							r:ClearAllPoints();
+							r:SetPoint("TOP", 0, 5);
+							break;
 						end 
 					end
 
-					local font, size, flag = _G[frameName.."Count"]:GetFont()
+					frame.icon:SetTexCoord(.08, .92, .08, .92);
+					frame.border:SetTexture("Interface\\Addons\\asDotFilter\\border.tga");
+					frame.border:SetTexCoord(0.08,0.08, 0.08,0.92, 0.92,0.08, 0.92,0.92);
 
-					_G[frameName.."Count"]:SetFont(font, ADotF_CountFontSize, "OUTLINE")
-					_G[frameName.."Count"]:SetPoint("BOTTOMRIGHT", -2, 2);
+					local font = frame.count:GetFont()
+
+					frame.count:SetFont(font, ADotF_CountFontSize, "OUTLINE")
+					frame.count:SetPoint("BOTTOMRIGHT", -2, 2);
 				end
 
 				-- set the icon
-				frameIcon = _G[frameName.."Icon"];
+				frameIcon = frame.icon;
 				frameIcon:SetTexture(icon);
 				frameIcon:SetAlpha(ADotF_ALPHA);
 
 				-- set the count
-				frameCount = _G[frameName.."Count"];
+				frameCount = frame.count;
 
 				-- Handle cooldowns
-				frameCooldown = _G[frameName.."Cooldown"];
-		
+				frameCooldown = frame.cooldown;
+			
 				if ( count > 1 ) then
 					frameCount:SetText(count);
 					frameCount:Show();
@@ -448,10 +414,9 @@ local function ADotF_UpdateDebuff(unit)
 					frameCooldown:SetDrawSwipe(true);
 				end
 
-		
 				if ( duration > 0 ) then
 					frameCooldown:Show();
-					CooldownFrame_Set(frameCooldown, expirationTime - duration, duration, duration > 0, true);
+					asCooldownFrame_Set(frameCooldown, expirationTime - duration, duration, duration > 0, true);
 					frameCooldown:SetHideCountdownNumbers(false);
 				else
 					frameCooldown:Hide();
@@ -463,132 +428,56 @@ local function ADotF_UpdateDebuff(unit)
 				else
 					color = DebuffTypeColor["none"];
 				end
-			
-				frameBorder = _G[frameName.."Border"];
+				
+				frameBorder = frame.border;
 				frameBorder:SetVertexColor(color.r, color.g, color.b);
 				frameBorder:SetAlpha(ADotF_ALPHA);
-							
+								
 				frame:ClearAllPoints();
 				frame:Show();
 
-				if (bAlert) then
+				if (((expirationTime - GetTime()) <= ADotF_ShowList[i][2]) and duration > 0) then
 					ADotF_ActionButton_ShowOverlayGlow(frame);
-					Prev_ExTime[unit][i] = nil;
 				else
 					ADotF_ActionButton_HideOverlayGlow(frame);
 				end
-
-
-				numDebuffs = numDebuffs + 1;
-			else
 				numDebuffs = numDebuffs + 1;
 			end
 		end
-	end
 
-	if (unit == "target")  then
 		for i=1, numDebuffs - 1 do
-			ADotF_UpdateDebuffAnchor(frametype, i, i - 1, ADotF_SIZE, 4, true, parent, powerbar);
-		end
-	else
-		for i=1, numDebuffs - 1 do
-			ADotF_UpdateDebuffAnchor(frametype, i, i - 1, ADotF_SIZE, 4, false, parent, powerbar);
+			ADotF_UpdateDebuffAnchor(ADotF.units[unit].frames, i, ADotF_SIZE, 2, true, parent, isboss);
 		end
 	end
-
+	
 	for i = numDebuffs, maxIdx do
-		frameName = frametype..i;
-		frame = _G[frameName];
+		frame = ADotF.units[unit].frames[i];
 
 		if ( frame ) then
 			ADotF_ActionButton_HideOverlayGlow(frame);
 			frame:Hide();	
 		end
-
-		prev_info[unit][i] =  {0, 0, 0, false};
-
 	end
 end
 
 
-local function ADotF_UpdateBossFrame()
-
-		unit = "boss1";
-
-		prev_info[unit] = {};
-
-		for i = 1, ADotF_MAX_DEBUFF_SHOW do 
-			prev_info[unit][i] = {0, 0, 0, false};
-		end
-		
-		ADotF_UpdateDebuff(unit);
-
-
-		unit = "boss2";
-
-		prev_info[unit] = {};
-
-		for i = 1, ADotF_MAX_DEBUFF_SHOW do 
-			prev_info[unit][i] =  {0, 0, 0, false};
-		end
-		
-		ADotF_UpdateDebuff(unit);
-
-		unit = "boss3";
-
-		prev_info[unit] = {};
-
-		for i = 1, ADotF_MAX_DEBUFF_SHOW do 
-			prev_info[unit][i] = {0, 0, 0, false};
-		end
-		
-		ADotF_UpdateDebuff(unit);
-
-		unit = "boss4";
-
-		prev_info[unit] = {};
-
-		for i = 1, ADotF_MAX_DEBUFF_SHOW do 
-			prev_info[unit][i] = {0, 0, 0, false};
-		end
-		
-		ADotF_UpdateDebuff(unit);
-
-		unit = "boss5";
-
-		prev_info[unit] = {};
-
-		for i = 1, ADotF_MAX_DEBUFF_SHOW do 
-			prev_info[unit][i] = {0, 0, 0, false};
-		end
-		
-		ADotF_UpdateDebuff(unit);
+local function ADotF_UpdateAllFrames()
+	for i = 1, #ADotF_UnitList do
+		ADotF_UpdateDebuff(ADotF_UnitList[i]);
+	end
 end
-
 
 local function ADotF_InitList()
 
 	local spec = GetSpecialization();
-	local localizedClass, englishClass = UnitClass("player")
-
+	local _, englishClass = UnitClass("player");
+	local listname = "ADotF_ShowList_";
 
 	if spec then
 		listname = "ADotF_ShowList_" .. englishClass .. "_" .. spec;
 	end
 
 	ADotF_ShowList = _G[listname];
-
-	for idx = 1, #ADotF_UnitList do
-		local unit = ADotF_UnitList[idx];
-
-		prev_info[unit] = {};
-
-		for i = 1, ADotF_MAX_DEBUFF_SHOW do 
-			prev_info[unit][i] =  {0, 0, 0, false};
-		end
-
-	end
-
 	ADotF_NameList = {};
 
 	if ADotF_ShowList then
@@ -603,62 +492,27 @@ local function ADotF_OnEvent(self, event, arg1)
 	local unit;
 
 	if (event == "PLAYER_FOCUS_CHANGED") then
-		
-		unit = "focus";
-
-		prev_info[unit] = {};
-
-		for i = 1, ADotF_MAX_DEBUFF_SHOW do 
-			prev_info[unit][i] =  {0, 0, 0, false};
-
-		end
-		
+		unit = "focus"
 		ADotF_UpdateDebuff(unit);
 	elseif (event == "PLAYER_TARGET_CHANGED") then
-			
 		unit = "target";
-
-		prev_info[unit] = {};
-
-		for i = 1, ADotF_MAX_DEBUFF_SHOW do 
-			prev_info[unit][i] =  {0, 0, 0, false};
-		end
-		
 		ADotF_UpdateDebuff(unit);
-
-	elseif (event == "INSTANCE_ENCOUNTER_ENGAGE_UNIT") then
-		
-		ADotF_UpdateBossFrame();
-
-
+	elseif (event == "INSTANCE_ENCOUNTER_ENGAGE_UNIT") then		
+		ADotF_UpdateAllFrames();
 	elseif (event == "ACTIONBAR_UPDATE_COOLDOWN") then
-
-		for i = 1, #ADotF_UnitList do
-			if arg1 == ADotF_UnitList[i] then
-				ADotF_UpdateDebuff(ADotF_UnitList[i]);
-			end
-		end
-
 	elseif (event == "PLAYER_ENTERING_WORLD") then
 		ADotF_InitList();
-		ADotF_UpdateBossFrame();
+		ADotF_UpdateAllFrames();
 	elseif (event == "ACTIVE_TALENT_GROUP_CHANGED") then
-		ADotF_InitList();
-		
+		ADotF_InitList();		
 	end
 end
 
 local function ADotF_OnUpdate()
-	for idx = 1, #ADotF_UnitList do
-
-		local unit = ADotF_UnitList[idx];
-		ADotF_UpdateDebuff(unit);
-	end
+	ADotF_UpdateAllFrames();
 end
 
 local function ADotF_Init()
-	
-
 	ADotF = CreateFrame("Frame", "ADotF", UIParent)
 
 	ADotF:SetPoint("CENTER", 0, 0)
@@ -668,15 +522,12 @@ local function ADotF_Init()
 	ADotF:Show()
 	ADotF:RegisterEvent("PLAYER_FOCUS_CHANGED")
 	ADotF:RegisterEvent("PLAYER_TARGET_CHANGED")
-	--ADotF:RegisterUnitEvent("UNIT_AURA", "target", "focus","boss1", "boss2", "boss3", "boss4", "boss5" )
 	ADotF:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
 	ADotF:RegisterEvent("PLAYER_ENTERING_WORLD")
 	ADotF:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
 	ADotF:RegisterEvent("ACTIONBAR_UPDATE_COOLDOWN")
 	ADotF:SetScript("OnEvent", ADotF_OnEvent)
-	C_Timer.NewTicker(0.5, ADotF_OnUpdate);
-	
+	C_Timer.NewTicker(0.2, ADotF_OnUpdate);	
 end
 
-ADotF_Init()
-
+ADotF_Init();
