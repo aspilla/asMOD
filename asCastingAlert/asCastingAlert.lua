98575e2e4f7288b552612165ca516daee52cf5ab
aspilla <aspilla@gmail.com>
2023-03-17 03:59:37 +0900

    신규 등록록

---
+++ asCastingAlert/asCastingAlert.lua
@@ -0,0 +1,118 @@
+﻿---설정부
+local ACTA_UpdateRate = 0.2			-- Check할 주기
+local ACTA_MaxShow = 3				-- 최대로 보여줄 개수
+local ACTA_FontSize = 18;
+local ACTA_X = 0;
+local ACTA_Y = -40;
+local needtowork = false;
+
+local function isFaction(unit)
+	if UnitIsUnit("player", unit) then
+		return false;		
+	else
+		local reaction = UnitReaction("player", unit);
+		if reaction and reaction <= 4 then
+			return true;		
+		elseif UnitIsPlayer(unit) then		
+			return false;
+		end
+	end
+end
+
+local currshow = 1;
+
+local function CheckCasting(nameplate)
+
+	if not nameplate or nameplate:IsForbidden()  then		
+		return false;
+	end
+
+	if not nameplate.UnitFrame or nameplate.UnitFrame:IsForbidden()  then		
+		return false;
+	end
+
+	local unit = nameplate.namePlateUnitToken;
+	
+	if isFaction(unit) and UnitIsUnit(unit .."target", "player") then
+		local name,  text, texture, startTime, endTime, isTradeSkill, castID, notInterruptible, spellid = UnitCastingInfo(unit);	
+		if not name then
+			name,  text, texture, startTime, endTime, isTradeSkill, notInterruptible, spellid = UnitChannelInfo(unit);
+		end
+
+		if name then
+
+			local curr = GetTime();
+			local remain = (endTime/1000) - curr;
+
+			if remain > 0 then
+				ACTA.cast[currshow]:SetText("|T"..texture..":0|t" .. format("%.1f", max(remain, 0)) .. "|T"..texture..":0|t");
+				if remain < 1.5 then
+					ACTA.cast[currshow]:SetTextColor(1, 0, 0);
+				else
+					ACTA.cast[currshow]:SetTextColor(1, 1, 1);
+				end
+
+				ACTA.cast[currshow]:Show();
+				currshow = currshow + 1;
+			end
+		end
+	end	
+end
+
+local function ACTA_OnUpdate()
+
+	currshow = 1;
+
+	for _,v in pairs(C_NamePlate.GetNamePlates(issecure())) do
+		local nameplate = v;
+		if (nameplate) then
+
+			CheckCasting(nameplate);
+			if currshow > ACTA_MaxShow then	
+				break;
+			end
+		end
+	end
+
+	for i = currshow, ACTA_MaxShow do
+		ACTA.cast[i]:Hide();
+	end
+end
+
+
+
+local function ACTA_OnEvent(self, event)
+
+	if event == "PLAYER_ENTERING_WORLD" then
+		needtowork = false;
+		local inInstance, instanceType = IsInInstance();
+
+		if (inInstance and instanceType == "party") then
+			needtowork = true;			
+		end	
+	end
+end
+
+
+local function initAddon()
+	ACTA = CreateFrame("Frame", nil, UIParent);
+	ACTA.cast = {};
+
+	for i = 1, ACTA_MaxShow do
+		ACTA.cast[i] = ACTA:CreateFontString(nil, "OVERLAY");
+		ACTA.cast[i]:SetFont("Fonts\\2002.TTF", ACTA_FontSize, "THICKOUTLINE")
+
+		if i == 1 then
+			ACTA.cast[i]:SetPoint("CENTER", UIParent, "CENTER", ACTA_X , ACTA_Y);
+		else
+			ACTA.cast[i]:SetPoint("CENTER", ACTA.cast[i - 1], "CENTER", 0 , -ACTA_FontSize - 3);
+		end
+		ACTA.cast[i]:Hide();
+	end
+
+	ACTA:RegisterEvent("PLAYER_ENTERING_WORLD");
+	ACTA:SetScript("OnEvent", ACTA_OnEvent)
+	--주기적으로 Callback
+	C_Timer.NewTicker(ACTA_UpdateRate, ACTA_OnUpdate);
+end
+initAddon();
