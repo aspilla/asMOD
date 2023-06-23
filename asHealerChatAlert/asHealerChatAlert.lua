dd74935c1467fb489356daa002caf26524db65ce
aspilla <aspilla@gmail.com>
2023-06-24 07:37:18 +0900

    마나 없을경우 안알리기

--- asHealerChatAlert/asHealerChatAlert.lua
+++ asHealerChatAlert/asHealerChatAlert.lua
@@ -1,10 +1,9 @@
-AHCA_Options_Default = {
+local AHCA = CreateFrame("FRAME", nil, UIParent);
+AHCA.unit = nil;
+AHCA.Options_Default = {
     AlertAnyway = false;            -- 무조건 알리기
 }
 
-
-local AHCA = CreateFrame("FRAME", nil, UIParent);
-AHCA.unit = nil;
 
 function AHCA:CheckHealer()
 
@@ -29,7 +28,7 @@ function AHCA:CheckHealer()
 				local unit = "party"..i;
                 local role = UnitGroupRolesAssigned(unit);
                 if role == "HEALER" then
-                    self.unit = unit;                   
+                    self.unit = unit;
                     return;
                 end
 			end
@@ -51,58 +50,57 @@ function AHCA:UpdateAlert()
 
     local max = UnitPowerMax(self.unit, Enum.PowerType.Mana );
     local mana = UnitPower(self.unit, Enum.PowerType.Mana );
-	if max and mana then
-        local  msg = "힐러마나 ".. math.ceil(mana/max * 100)  .. "%";        
+	if max and mana and mana > 0 and max > 0 then
+        local  msg = "힐러마나 ".. math.ceil(mana/max * 100)  .. "%";
         --print (msg);
         SendChatMessage(msg);
     end
 end
 
-local bfirst = false;
+function AHCA.OnEvent(self, event, ...)
 
-local function AHCA_OnEvent(self, event, ...)
-
-    if not bfirst then
-        self:SetupOptionPanels();
-        bfirst = true;
-    end
     self:CheckHealer();
 	if event == "PLAYER_REGEN_ENABLED" then
 		self:UpdateAlert();
 	end
 end
 
+function AHCA:OnInit()
 
-AHCA:SetScript("OnEvent", AHCA_OnEvent);
-AHCA:RegisterEvent("PLAYER_REGEN_ENABLED");
-AHCA:RegisterEvent("PLAYER_ENTERING_WORLD");
-AHCA:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED");
-AHCA:RegisterEvent("GROUP_ROSTER_UPDATE");
-AHCA:RegisterEvent("ROLE_CHANGED_INFORM");
+    self:SetupOptionPanels();
 
 
-local function OnSettingChanged(_, setting, value)
-	local variable = setting:GetVariable()
-	AHCA_Options[variable] = value;
-    ReloadUI();
+    self:SetScript("OnEvent", self.OnEvent);
+    self:RegisterEvent("PLAYER_REGEN_ENABLED");
+    self:RegisterEvent("PLAYER_ENTERING_WORLD");
+    self:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED");
+    self:RegisterEvent("GROUP_ROSTER_UPDATE");
+    self:RegisterEvent("ROLE_CHANGED_INFORM");
+
 end
 
 function AHCA:SetupOptionPanels()
+
+    local function OnSettingChanged(_, setting, value)
+        local variable = setting:GetVariable()
+        AHCA_Options[variable] = value;
+        ReloadUI();
+    end
 
     local category = Settings.RegisterVerticalLayoutCategory("asHealthChatAlert")
 
     if AHCA_Options == nil then
         AHCA_Options = {};
-        AHCA_Options = CopyTable(AHCA_Options_Default);
+        AHCA_Options = CopyTable(self.Options_Default);
     end
 
-    for variable, _ in pairs(AHCA_Options_Default) do
+    for variable, _ in pairs(self.Options_Default) do
 
-        
+
         local name = variable;
         local tooltip = ""
-        if  AHCA_Options[variable] == nil then    
-            AHCA_Options[variable] = AHCA_Options_Default[variable];
+        if  AHCA_Options[variable] == nil then
+            AHCA_Options[variable] = self.Options_Default[variable];
         end
         local defaultValue = AHCA_Options[variable];
 
@@ -113,3 +111,5 @@ function AHCA:SetupOptionPanels()
 
     Settings.RegisterAddOnCategory(category)
 end
+
+AHCA:OnInit();
