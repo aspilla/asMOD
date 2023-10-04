0cff484ecb0b45d94a76e6b49205cf2a5e8e9329
aspilla <aspilla@gmail.com>
2023-10-05 07:18:54 +0900

    옵션 분리

--- asCompactRaidBuff/asCompactRaidBuff.lua
+++ asCompactRaidBuff/asCompactRaidBuff.lua
@@ -1,242 +1,13 @@
 local _, ns = ...;
-local ACRB_BuffSizeRate = 0.9;        -- 기존 Size 크기 배수
-local ACRB_ShowBuffCooldown = false   -- 버프 지속시간을 보이려면
-local ACRB_MinShowBuffFontSize = 5    -- 이크기보다 Cooldown font Size 가 작으면 안보이게 한다. 무조건 보이게 하려면 0
-local ACRB_CooldownFontSizeRate = 0.5 -- 버프 Size 대비 쿨다운 폰트 사이즈
+
 local ACRB_MAX_BUFFS = 6              -- 최대 표시 버프 개수 (3개 + 3개)
 local ACRB_MAX_BUFFS_2 = 2            -- 최대 생존기 개수
 local ACRB_MAX_DEBUFFS = 3            -- 최대 표시 디버프 개수 (3개)
 local ACRB_MAX_DISPELDEBUFFS = 3      -- 최대 해제 디버프 개수 (3개)
 local ACRB_MAX_CASTING = 2            -- 최대 Casting Alert
-local ACRB_ShowAlert = true           -- HOT 리필 시 알림
 local ACRB_MaxBuffSize = 20           -- 최대 Buff Size 창을 늘려도 이 크기 이상은 안커짐
 local ACRB_HealerManaBarHeight = 3    -- 힐러 마나바 크기 (안보이게 하려면 0)
-local ACRB_UpdateRate = (0.2)         -- 1회 Update 주기 (초) 작으면 작을 수록 Frame Rate 감소 가능, 크면 Update 가 느림
-local ACRB_ShowWhenSolo = true        -- Solo Raid Frame 사용시 보이게 하려면 True (반드시 Solo Raid Frame과 사용)
-local ACRB_ShowTooltip = true         -- GameTooltip을 보이게 하려면 True
 
--- 버프 남은시간에 리필 알림
--- 두번째 숫자는 표시 위치, 4(우상) 5(우중) 6(우중2) 7(상바) 1,2,3 은 우하에 보이는 우선 순위이다.
-ACRB_ShowList_MONK_2 = {
-	["소생의 안개"] = { 0, 4 },
-	["포용의 안개"] = { 0, 5 },
-
-
-}
-
--- 신기
-ACRB_ShowList_PALADIN_1 = {
-	["빛의 자락"] = { 0, 4 },
-	["빛의 봉화"] = { 0, 5 },
-	["신념의 봉화"] = { 0, 6 },
-}
-
--- 수사
-ACRB_ShowList_PRIEST_1 = {
-	["속죄"] = { 0, 4 },
-	["소생"] = { 1, 5 },
-	["신의 권능: 보호막"] = { 0, 6 },
-	["회복의 기원"] = { 0, 2 },
-	["마력 주입"] = { 0, 1 },
-
-}
-
-
--- 신사
-ACRB_ShowList_PRIEST_2 = {
-	["소생"] = { 1, 4 },
-	["신의 권능: 보호막"] = { 0, 5 },
-	["회복의 기원"] = { 0, 2 },
-	["마력 주입"] = { 0, 1 },
-
-}
-
-ACRB_ShowList_PRIEST_3 = {
-	["마력 주입"] = { 0, 1 },
-}
-
-
-ACRB_ShowList_SHAMAN_3 = {
-	["성난 해일"] = { 1, 4 },
-}
-
-
-ACRB_ShowList_DRUID_4 = {
-	["회복"] = { 1, 4 },
-	["피어나는 생명"] = { 1, 5 },
-	["재생"] = { 1, 6 },
-	["회복 (싹틔우기)"] = { 1, 2 },
-	["세나리온 수호물"] = { 0, 1 },
-}
-
-ACRB_ShowList_EVOKER_2 = {
-	["메아리"] = { 0, 4 },
-	["되감기"] = { 1, 5 },
-
-}
-
-
--- 안보이게 할 디법
-local ACRB_BlackList = {
-	["도전자의 짐"] = 1,
-}
-
-
--- 해제 알림 스킬
-local ACRB_DispelAlertList = {
-	--["탈진"] = 1,	
-}
-
-
-
---직업별 생존기 등록 (10초 쿨다운), 용군단 Version
-local ACRB_PVPBuffList = {
-	[236273] = true, --WARRIOR
-	[213871] = true, --WARRIOR
-	[118038] = true, --WARRIOR
-	[12975] = true, --WARRIOR
-	[1160] = true, --WARRIOR
-	[871] = true, --WARRIOR
-	[202168] = true, --WARRIOR
-	[97463] = true, --WARRIOR
-	[383762] = true, --WARRIOR
-	[184364] = true, --WARRIOR
-	[386394] = true, --WARRIOR
-	[392966] = true, --WARRIOR
-	[185311] = true, --ROGUE
-	[11327] = true, --ROGUE
-	[1966] = true, --ROGUE
-	[31224] = true, --ROGUE
-	[31230] = true, --ROGUE
-	[5277] = true, --ROGUE
-	[212800] = true, --DEMONHUNTER
-	[203720] = true, --DEMONHUNTER
-	[187827] = true, --DEMONHUNTER
-	[206803] = true, --DEMONHUNTER
-	[196555] = true, --DEMONHUNTER
-	[204021] = true, --DEMONHUNTER
-	[263648] = true, --DEMONHUNTER
-	[209258] = true, --DEMONHUNTER
-	[209426] = true, --DEMONHUNTER
-	[202162] = true, --MONK
-	[388615] = true, --MONK
-	[115310] = true, --MONK
-	[116849] = true, --MONK
-	[115399] = true, --MONK
-	[119582] = true, --MONK
-	[122281] = true, --MONK
-	[322507] = true, --MONK
-	[120954] = true, --MONK
-	[122783] = true, --MONK
-	[122278] = true, --MONK
-	[132578] = true, --MONK
-	[115176] = true, --MONK
-	[51052] = true, --DEATHKNIGHT
-	[48707] = true, --DEATHKNIGHT
-	[327574] = true, --DEATHKNIGHT
-	[48743] = true, --DEATHKNIGHT
-	[48792] = true, --DEATHKNIGHT
-	[114556] = true, --DEATHKNIGHT
-	[81256] = true, --DEATHKNIGHT
-	[219809] = true, --DEATHKNIGHT
-	[206931] = true, --DEATHKNIGHT
-	[274156] = true, --DEATHKNIGHT
-	[194679] = true, --DEATHKNIGHT
-	[55233] = true, --DEATHKNIGHT
-	[272679] = true, --HUNTER
-	[53480] = true, --HUNTER
-	[109304] = true, --HUNTER
-	[264735] = true, --HUNTER
-	[186265] = true, --HUNTER
-	[355913] = true, --EVOKER
-	[370960] = true, --EVOKER
-	[363534] = true, --EVOKER
-	[357170] = true, --EVOKER
-	[374348] = true, --EVOKER
-	[374227] = true, --EVOKER
-	[363916] = true, --EVOKER
-	[360827] = true, --EVOKER
-	[404381] = true, --EVOKER
-	[305497] = true, --DRUID
-	[354654] = true, --DRUID
-	[201664] = true, --DRUID
-	[157982] = true, --DRUID
-	[102342] = true, --DRUID
-	[61336] = true, --DRUID
-	[200851] = true, --DRUID
-	[80313] = true, --DRUID
-	[22842] = true, --DRUID
-	[108238] = true, --DRUID
-	[124974] = true, --DRUID
-	[22812] = true, --DRUID
-	[104773] = true, --WARLOCK
-	[108416] = true, --WARLOCK
-	[215769] = true, --PRIEST
-	[328530] = true, --PRIEST
-	[197268] = true, --PRIEST
-	[19236] = true, --PRIEST
-	[81782] = true, --PRIEST
-	[33206] = true, --PRIEST
-	[372835] = true, --PRIEST
-	[391124] = true, --PRIEST
-	[265202] = true, --PRIEST
-	[64843] = true, --PRIEST
-	[47788] = true, --PRIEST
-	[47585] = true, --PRIEST
-	[108968] = true, --PRIEST
-	[15286] = true, --PRIEST
-	[271466] = true, --PRIEST
-	[199452] = true, --PALADIN
-	[403876] = true, --PALADIN
-	[31850] = true, --PALADIN
-	[378279] = true, --PALADIN
-	[378974] = true, --PALADIN
-	[86659] = true, --PALADIN
-	[387174] = true, --PALADIN
-	[327193] = true, --PALADIN
-	[205191] = true, --PALADIN
-	[184662] = true, --PALADIN
-	[498] = true, --PALADIN
-	[148039] = true, --PALADIN
-	[157047] = true, --PALADIN
-	[31821] = true, --PALADIN
-	[633] = true, --PALADIN
-	[6940] = true, --PALADIN
-	[1022] = true, --PALADIN
-	[204018] = true, --PALADIN
-	[204331] = true, --SHAMAN
-	[108280] = true, --SHAMAN
-	[98008] = true, --SHAMAN
-	[198838] = true, --SHAMAN
-	[207399] = true, --SHAMAN
-	[108271] = true, --SHAMAN
-	[198103] = true, --SHAMAN
-	[30884] = true, --SHAMAN
-	[383017] = true, --SHAMAN
-	[108281] = true, --SHAMAN
-	[198111] = true, --MAGE
-	[110959] = true, --MAGE
-	[342246] = true, --MAGE
-	[11426] = true, --MAGE
-	[66] = true,  --MAGE
-	[235313] = true, --MAGE
-	[235450] = true, --MAGE
-	[55342] = true, --MAGE
-	[414660] = true, --MAGE
-	[414664] = true, --MAGE
-	[86949] = true, --MAGE
-	[235219] = true, --MAGE
-	[414658] = true, --MAGE
-	[110960] = true, --MAGE
-	[125174] = true, --MONK
-	[186265] = true, --HUNTER
-	[378441] = true, --EVOKER
-	[228049] = true, --PALADIN
-	[642] = true, --PALADIN
-	[409293] = true, --SHAMAN
-	[45438] = true, --MAGE	
-	[586] = true, --PRIEST
-}
 
 --Overlay stuff
 
@@ -464,7 +235,7 @@ local function ACRB_InitList()
 		listname = "ACRB_ShowList_" .. englishClass .. "_" .. spec;
 	end
 
-	ACRB_ShowList = _G[listname];
+	ACRB_ShowList = ns[listname];
 end
 
 -- 버프 설정 부
@@ -905,7 +676,7 @@ local function ProcessAura(aura)
 		return AuraUpdateChangedType.None;
 	end
 
-	if ACRB_BlackList and ACRB_BlackList[aura.name] then
+	if ns.ACRB_ShowList and ns.ACRB_ShowList[aura.name] then
 		return AuraUpdateChangedType.None;
 	end
 
@@ -921,7 +692,7 @@ local function ProcessAura(aura)
 			aura.debuffType = UnitFrameDebuffType.NonBossDebuff;
 			return AuraUpdateChangedType.Debuff;
 		end
-	elseif aura.isHelpful and ACRB_PVPBuffList[aura.spellId] then
+	elseif aura.isHelpful and ns.ACRB_PVPBuffList[aura.spellId] then
 		return AuraUpdateChangedType.PVP;
 	elseif aura.isHelpful and ShouldDisplayBuff(aura) then
 		aura.isBuff = true;
@@ -1311,10 +1082,10 @@ local function ACRB_setupFrame(frame)
 
 	y = y - powerBarUsedHeight;
 
-	local size_x = x / 6 * ACRB_BuffSizeRate - 1;
-	local size_y = y / 3 * ACRB_BuffSizeRate - 1;
+	local size_x = x / 6 * ns.ACRB_BuffSizeRate - 1;
+	local size_y = y / 3 * ns.ACRB_BuffSizeRate - 1;
 
-	local baseSize = math.min(x / 7 * ACRB_BuffSizeRate, y / 3 * ACRB_BuffSizeRate);
+	local baseSize = math.min(x / 7 * ns.ACRB_BuffSizeRate, y / 3 * ns.ACRB_BuffSizeRate);
 
 	if baseSize > ACRB_MaxBuffSize then
 		baseSize = ACRB_MaxBuffSize
@@ -1322,7 +1093,7 @@ local function ACRB_setupFrame(frame)
 
 	baseSize = baseSize * 0.9;
 
-	local fontsize = baseSize * ACRB_CooldownFontSizeRate;
+	local fontsize = baseSize * ns.ACRB_MinShowBuffFontSize;
 
 	if asraid[frameName].isDispellAlert == nil then
 		asraid[frameName].isDispellAlert = false;
@@ -1359,7 +1130,7 @@ local function ACRB_setupFrame(frame)
 		asraid[frameName].asbuffFrames = {}
 		for i = 1, ACRB_MAX_BUFFS do
 			local buffFrame = CreateFrame("Button", nil, frame, "asCompactBuffTemplate")
-			buffFrame:EnableMouse(ACRB_ShowTooltip);
+			buffFrame:EnableMouse(ns.ACRB_ShowTooltip);
 			buffFrame.icon:SetTexCoord(.08, .92, .08, .92);
 			buffFrame.border:SetTexture("Interface\\Addons\\asCompactRaidBuff\\border.tga");
 			buffFrame.border:SetTexCoord(0.08, 0.08, 0.08, 0.92, 0.92, 0.08, 0.92, 0.92);
@@ -1375,7 +1146,7 @@ local function ACRB_setupFrame(frame)
 			local buffFrame = asraid[frameName].asbuffFrames[i];
 			buffFrame:ClearAllPoints()
 
-			if ACRB_ShowTooltip and not buffFrame:GetScript("OnEnter") then
+			if ns.ACRB_ShowTooltip and not buffFrame:GetScript("OnEnter") then
 				buffFrame:SetScript("OnEnter", function(s)
 					if s.auraInstanceID then
 						GameTooltip_SetDefaultAnchor(GameTooltip, s);
@@ -1431,7 +1202,7 @@ local function ACRB_setupFrame(frame)
 		d.count:SetFont(STANDARD_TEXT_FONT, fontsize, "OUTLINE")
 		d.count:ClearAllPoints();
 		d.count:SetPoint("BOTTOM", 0, 1);
-		if ACRB_ShowBuffCooldown and fontsize >= ACRB_MinShowBuffFontSize then
+		if ns.ACRB_ShowBuffCooldown and fontsize >= ns.ACRB_MinShowBuffFontSize then
 			d.cooldown:SetHideCountdownNumbers(false);
 			for _, r in next, { d.cooldown:GetRegions() } do
 				if r:GetObjectType() == "FontString" then
@@ -1448,7 +1219,7 @@ local function ACRB_setupFrame(frame)
 		asraid[frameName].asdebuffFrames = {};
 		for i = 1, ACRB_MAX_DEBUFFS do
 			local debuffFrame = CreateFrame("Button", nil, frame, "asCompactDebuffTemplate")
-			debuffFrame:EnableMouse(ACRB_ShowTooltip);
+			debuffFrame:EnableMouse(ns.ACRB_ShowTooltip);
 			debuffFrame.icon:SetTexCoord(.08, .92, .08, .92);
 			debuffFrame.border:SetTexture("Interface\\Addons\\asCompactRaidBuff\\border.tga");
 			debuffFrame.border:SetTexCoord(0.08, 0.08, 0.08, 0.92, 0.92, 0.08, 0.92, 0.92);
@@ -1475,7 +1246,7 @@ local function ACRB_setupFrame(frame)
 				debuffFrame:SetPoint("BOTTOMLEFT", asraid[frameName].asdebuffFrames[i - 1], "BOTTOMRIGHT", 1, 0)
 			end
 
-			if ACRB_ShowTooltip and not debuffFrame:GetScript("OnEnter") then
+			if ns.ACRB_ShowTooltip and not debuffFrame:GetScript("OnEnter") then
 				debuffFrame:SetScript("OnEnter", function(s)
 					if s.auraInstanceID then
 						GameTooltip_SetDefaultAnchor(GameTooltip, s);
@@ -1498,7 +1269,7 @@ local function ACRB_setupFrame(frame)
 		d.count:ClearAllPoints();
 		d.count:SetPoint("BOTTOM", 0, 1);
 
-		if ACRB_ShowBuffCooldown and fontsize >= ACRB_MinShowBuffFontSize then
+		if ns.ACRB_ShowBuffCooldown and fontsize >= ns.ACRB_MinShowBuffFontSize then
 			d.cooldown:SetHideCountdownNumbers(false);
 			for _, r in next, { d.cooldown:GetRegions() } do
 				if r:GetObjectType() == "FontString" then
@@ -1561,14 +1332,14 @@ local function ACRB_setupFrame(frame)
 
 		for i = 1, ACRB_MAX_BUFFS_2 do
 			asraid[frameName].pvpbuffFrames[i] = CreateFrame("Button", nil, frame, "asCompactBuffTemplate")
-			asraid[frameName].pvpbuffFrames[i]:EnableMouse(ACRB_ShowTooltip);
+			asraid[frameName].pvpbuffFrames[i]:EnableMouse(ns.ACRB_ShowTooltip);
 			asraid[frameName].pvpbuffFrames[i].icon:SetTexCoord(.08, .92, .08, .92);
 			asraid[frameName].pvpbuffFrames[i].count:SetFont(STANDARD_TEXT_FONT, fontsize, "OUTLINE");
 			asraid[frameName].pvpbuffFrames[i].count:ClearAllPoints();
 			asraid[frameName].pvpbuffFrames[i].count:SetPoint("BOTTOM", 0, 0);
 			asraid[frameName].pvpbuffFrames[i]:Hide();
 
-			if ACRB_ShowBuffCooldown and fontsize >= ACRB_MinShowBuffFontSize then
+			if ns.ACRB_ShowBuffCooldown and fontsize >= ns.ACRB_MinShowBuffFontSize then
 				asraid[frameName].pvpbuffFrames[i].cooldown:SetHideCountdownNumbers(false);
 				for _, r in next, { asraid[frameName].pvpbuffFrames[i].cooldown:GetRegions() } do
 					if r:GetObjectType() == "FontString" then
@@ -1601,7 +1372,7 @@ local function ACRB_setupFrame(frame)
 					0)
 			end
 
-			if ACRB_ShowTooltip and not asraid[frameName].pvpbuffFrames[i]:GetScript("OnEnter") then
+			if ns.ACRB_ShowTooltip and not asraid[frameName].pvpbuffFrames[i]:GetScript("OnEnter") then
 				asraid[frameName].pvpbuffFrames[i]:SetScript("OnEnter", function(s)
 					if s.auraInstanceID then
 						GameTooltip_SetDefaultAnchor(GameTooltip, s);
@@ -1623,14 +1394,14 @@ local function ACRB_setupFrame(frame)
 
 		for i = 1, ACRB_MAX_CASTING do
 			asraid[frameName].castFrames[i] = CreateFrame("Button", nil, frame, "asCompactBuffTemplate")
-			asraid[frameName].castFrames[i]:EnableMouse(ACRB_ShowTooltip);
+			asraid[frameName].castFrames[i]:EnableMouse(ns.ACRB_ShowTooltip);
 			asraid[frameName].castFrames[i].icon:SetTexCoord(.08, .92, .08, .92);
 			asraid[frameName].castFrames[i].count:SetFont(STANDARD_TEXT_FONT, fontsize, "OUTLINE");
 			asraid[frameName].castFrames[i].count:ClearAllPoints();
 			asraid[frameName].castFrames[i].count:SetPoint("BOTTOM", 0, 0);
 			asraid[frameName].castFrames[i]:Hide();
 
-			if ACRB_ShowBuffCooldown and fontsize >= ACRB_MinShowBuffFontSize then
+			if ns.ACRB_ShowBuffCooldown and fontsize >= ns.ACRB_MinShowBuffFontSize then
 				asraid[frameName].castFrames[i].cooldown:SetHideCountdownNumbers(false);
 				for _, r in next, { asraid[frameName].castFrames[i].cooldown:GetRegions() } do
 					if r:GetObjectType() == "FontString" then
@@ -1644,7 +1415,7 @@ local function ACRB_setupFrame(frame)
 				asraid[frameName].castFrames[i].cooldown:SetHideCountdownNumbers(true);
 			end
 
-			if ACRB_ShowTooltip and not asraid[frameName].castFrames[i]:GetScript("OnEnter") then
+			if ns.ACRB_ShowTooltip and not asraid[frameName].castFrames[i]:GetScript("OnEnter") then
 				asraid[frameName].castFrames[i]:SetScript("OnEnter", function(s)
 					if s.castspellid and s.castspellid > 0 then
 						GameTooltip_SetDefaultAnchor(GameTooltip, s);
@@ -1758,7 +1529,7 @@ ACRB_mainframe:RegisterEvent("VARIABLES_LOADED");
 ACRB_mainframe:RegisterEvent("PLAYER_REGEN_ENABLED");
 ACRB_mainframe:RegisterEvent("PLAYER_REGEN_DISABLED");
 
-C_Timer.NewTicker(ACRB_UpdateRate, ACRB_OnUpdate);
+C_Timer.NewTicker(ns.ACRB_UpdateRate, ACRB_OnUpdate);
 
 hooksecurefunc("CompactUnitFrame_UpdateAll", ARCB_UpdateAll);
 hooksecurefunc("CompactUnitFrame_UpdateName", UpdateNameColor);
---
+++ asCompactRaidBuff/asCompactRaidBuffOptions.lua
@@ -0,0 +1,224 @@
+local _, ns = ...;
+
+ns.ACRB_BuffSizeRate = 0.9;        -- 기존 Size 크기 배수
+ns.ACRB_ShowBuffCooldown = false   -- 버프 지속시간을 보이려면
+ns.ACRB_MinShowBuffFontSize = 5    -- 이크기보다 Cooldown font Size 가 작으면 안보이게 한다. 무조건 보이게 하려면 0
+ns.ACRB_MinShowBuffFontSize = 0.5 -- 버프 Size 대비 쿨다운 폰트 사이즈
+ns.ACRB_UpdateRate = (0.2)         -- 1회 Update 주기 (초) 작으면 작을 수록 Frame Rate 감소 가능, 크면 Update 가 느림
+ns.ACRB_ShowTooltip = true         -- GameTooltip을 보이게 하려면 True
+
+-- 버프 남은시간에 리필 알림
+-- 두번째 숫자는 표시 위치, 4(우상) 5(우중) 6(우중2) 7(상바) 1,2,3 은 우하에 보이는 우선 순위이다.
+ns.ACRB_ShowList_MONK_2 = {
+	["소생의 안개"] = { 0, 4 },
+	["포용의 안개"] = { 0, 5 },
+
+
+}
+
+-- 신기
+ns.ACRB_ShowList_PALADIN_1 = {
+	["빛의 자락"] = { 0, 4 },
+	["빛의 봉화"] = { 0, 5 },
+	["신념의 봉화"] = { 0, 6 },
+}
+
+-- 수사
+ns.ACRB_ShowList_PRIEST_1 = {
+	["속죄"] = { 0, 4 },
+	["소생"] = { 1, 5 },
+	["신의 권능: 보호막"] = { 0, 6 },
+	["회복의 기원"] = { 0, 2 },
+	["마력 주입"] = { 0, 1 },
+
+}
+
+
+-- 신사
+ns.ACRB_ShowList_PRIEST_2 = {
+	["소생"] = { 1, 4 },
+	["신의 권능: 보호막"] = { 0, 5 },
+	["회복의 기원"] = { 0, 2 },
+	["마력 주입"] = { 0, 1 },
+
+}
+
+ns.ACRB_ShowList_PRIEST_3 = {
+	["마력 주입"] = { 0, 1 },
+}
+
+
+ns.ACRB_ShowList_SHAMAN_3 = {
+	["성난 해일"] = { 1, 4 },
+}
+
+
+ns.ACRB_ShowList_DRUID_4 = {
+	["회복"] = { 1, 4 },
+	["피어나는 생명"] = { 1, 5 },
+	["재생"] = { 1, 6 },
+	["회복 (싹틔우기)"] = { 1, 2 },
+	["세나리온 수호물"] = { 0, 1 },
+}
+
+ns.ACRB_ShowList_EVOKER_2 = {
+	["메아리"] = { 0, 4 },
+	["되감기"] = { 1, 5 },
+
+}
+
+
+-- 안보이게 할 디법
+ns.ACRB_ShowList = {
+	["도전자의 짐"] = 1,
+}
+
+
+--직업별 생존기 등록 (10초 쿨다운), 용군단 Version
+ns.ACRB_PVPBuffList = {
+	[236273] = true, --WARRIOR
+	[213871] = true, --WARRIOR
+	[118038] = true, --WARRIOR
+	[12975] = true, --WARRIOR
+	[1160] = true, --WARRIOR
+	[871] = true, --WARRIOR
+	[202168] = true, --WARRIOR
+	[97463] = true, --WARRIOR
+	[383762] = true, --WARRIOR
+	[184364] = true, --WARRIOR
+	[386394] = true, --WARRIOR
+	[392966] = true, --WARRIOR
+	[185311] = true, --ROGUE
+	[11327] = true, --ROGUE
+	[1966] = true, --ROGUE
+	[31224] = true, --ROGUE
+	[31230] = true, --ROGUE
+	[5277] = true, --ROGUE
+	[212800] = true, --DEMONHUNTER
+	[203720] = true, --DEMONHUNTER
+	[187827] = true, --DEMONHUNTER
+	[206803] = true, --DEMONHUNTER
+	[196555] = true, --DEMONHUNTER
+	[204021] = true, --DEMONHUNTER
+	[263648] = true, --DEMONHUNTER
+	[209258] = true, --DEMONHUNTER
+	[209426] = true, --DEMONHUNTER
+	[202162] = true, --MONK
+	[388615] = true, --MONK
+	[115310] = true, --MONK
+	[116849] = true, --MONK
+	[115399] = true, --MONK
+	[119582] = true, --MONK
+	[122281] = true, --MONK
+	[322507] = true, --MONK
+	[120954] = true, --MONK
+	[122783] = true, --MONK
+	[122278] = true, --MONK
+	[132578] = true, --MONK
+	[115176] = true, --MONK
+	[51052] = true, --DEATHKNIGHT
+	[48707] = true, --DEATHKNIGHT
+	[327574] = true, --DEATHKNIGHT
+	[48743] = true, --DEATHKNIGHT
+	[48792] = true, --DEATHKNIGHT
+	[114556] = true, --DEATHKNIGHT
+	[81256] = true, --DEATHKNIGHT
+	[219809] = true, --DEATHKNIGHT
+	[206931] = true, --DEATHKNIGHT
+	[274156] = true, --DEATHKNIGHT
+	[194679] = true, --DEATHKNIGHT
+	[55233] = true, --DEATHKNIGHT
+	[272679] = true, --HUNTER
+	[53480] = true, --HUNTER
+	[109304] = true, --HUNTER
+	[264735] = true, --HUNTER
+	[186265] = true, --HUNTER
+	[355913] = true, --EVOKER
+	[370960] = true, --EVOKER
+	[363534] = true, --EVOKER
+	[357170] = true, --EVOKER
+	[374348] = true, --EVOKER
+	[374227] = true, --EVOKER
+	[363916] = true, --EVOKER
+	[360827] = true, --EVOKER
+	[404381] = true, --EVOKER
+	[305497] = true, --DRUID
+	[354654] = true, --DRUID
+	[201664] = true, --DRUID
+	[157982] = true, --DRUID
+	[102342] = true, --DRUID
+	[61336] = true, --DRUID
+	[200851] = true, --DRUID
+	[80313] = true, --DRUID
+	[22842] = true, --DRUID
+	[108238] = true, --DRUID
+	[124974] = true, --DRUID
+	[22812] = true, --DRUID
+	[104773] = true, --WARLOCK
+	[108416] = true, --WARLOCK
+	[215769] = true, --PRIEST
+	[328530] = true, --PRIEST
+	[197268] = true, --PRIEST
+	[19236] = true, --PRIEST
+	[81782] = true, --PRIEST
+	[33206] = true, --PRIEST
+	[372835] = true, --PRIEST
+	[391124] = true, --PRIEST
+	[265202] = true, --PRIEST
+	[64843] = true, --PRIEST
+	[47788] = true, --PRIEST
+	[47585] = true, --PRIEST
+	[108968] = true, --PRIEST
+	[15286] = true, --PRIEST
+	[271466] = true, --PRIEST
+	[199452] = true, --PALADIN
+	[403876] = true, --PALADIN
+	[31850] = true, --PALADIN
+	[378279] = true, --PALADIN
+	[378974] = true, --PALADIN
+	[86659] = true, --PALADIN
+	[387174] = true, --PALADIN
+	[327193] = true, --PALADIN
+	[205191] = true, --PALADIN
+	[184662] = true, --PALADIN
+	[498] = true, --PALADIN
+	[148039] = true, --PALADIN
+	[157047] = true, --PALADIN
+	[31821] = true, --PALADIN
+	[633] = true, --PALADIN
+	[6940] = true, --PALADIN
+	[1022] = true, --PALADIN
+	[204018] = true, --PALADIN
+	[204331] = true, --SHAMAN
+	[108280] = true, --SHAMAN
+	[98008] = true, --SHAMAN
+	[198838] = true, --SHAMAN
+	[207399] = true, --SHAMAN
+	[108271] = true, --SHAMAN
+	[198103] = true, --SHAMAN
+	[30884] = true, --SHAMAN
+	[383017] = true, --SHAMAN
+	[108281] = true, --SHAMAN
+	[198111] = true, --MAGE
+	[110959] = true, --MAGE
+	[342246] = true, --MAGE
+	[11426] = true, --MAGE
+	[66] = true,  --MAGE
+	[235313] = true, --MAGE
+	[235450] = true, --MAGE
+	[55342] = true, --MAGE
+	[414660] = true, --MAGE
+	[414664] = true, --MAGE
+	[86949] = true, --MAGE
+	[235219] = true, --MAGE
+	[414658] = true, --MAGE
+	[110960] = true, --MAGE
+	[125174] = true, --MONK
+	[186265] = true, --HUNTER
+	[378441] = true, --EVOKER
+	[228049] = true, --PALADIN
+	[642] = true, --PALADIN
+	[409293] = true, --SHAMAN
+	[45438] = true, --MAGE	
+	[586] = true, --PRIEST
+}
