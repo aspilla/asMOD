53ba40b163f09ca54fe1802da73307b3e7402feb
aspilla <aspilla@gmail.com>
2025-03-18 18:13:48 +0900

    Adding asHideNamePlates

---
+++ asHideNamePlates/asHideNamePlates.lua
@@ -0,0 +1,268 @@
+﻿local _, ns = ...;
+
+---설정부
+local AHNameP_UpdateRate = 0.2 -- Check할 주기
+
+local AHNameP_AlertList = {
+	["폭발물"] = true,
+}
+
+local DangerousSpellList = {
+
+}
+
+local usePlater = C_AddOns.LoadAddOn("Plater");
+
+local function getUnitFrame(nameplate)
+	if usePlater then
+		return nameplate.unitFrame
+	else
+		return nameplate.UnitFrame
+	end
+end
+
+local function isFaction(unit)
+	if UnitIsUnit("player", unit) then
+		return false;
+	else
+		local reaction = UnitReaction("player", unit);
+		if reaction and reaction <= 4 then
+			return true;
+		elseif UnitIsPlayer(unit) then
+			return false;
+		end
+	end
+end
+
+local function isShow(unit)
+
+	if not isFaction(unit) then
+		return false;
+	end
+
+	local unitname = GetUnitName(unit);
+
+	if unitname and AHNameP_AlertList[unitname] then
+		return true;
+	end
+
+	if UnitIsUnit(unit, "target") then
+		return false;
+	end
+
+	local name, _, _, _, _, _, _, _, spellid = UnitCastingInfo(unit);
+	if not name then
+		name, _, _, _, _, _, _, _, spellid = UnitChannelInfo(unit);
+	end
+
+	local status = UnitThreatSituation("player", unit);
+
+	if ns.options.Check_DBM_Interrupt_Only then
+		-- 차단 가능 스킬 모두 발동
+		if name and spellid and status and DangerousSpellList[spellid] then
+			return true;
+		end
+	else
+		if name and spellid and status then
+			return true;
+		end
+	end
+
+	return false;
+end
+
+-- Mouse Button 4 상태를 저장할 변수 추가
+AHNP_mouseButton4Pressed = false;
+
+local function checkNeedtoHide(nameplate)
+	if not nameplate or nameplate:IsForbidden() then
+		return false;
+	end
+
+	if not nameplate.UnitFrame or nameplate.UnitFrame:IsForbidden() then
+		return false;
+	end
+
+	local unit = nameplate.namePlateUnitToken;
+
+	if ns.options.HideModifier == 1 then
+		if AHNP_mouseButton4Pressed then
+			return true;
+		else
+			return false;
+		end
+	elseif ns.options.HideModifier < 6 then
+		if (ns.options.HideModifier == 2 and IsAltKeyDown() and IsControlKeyDown()) or
+			(ns.options.HideModifier == 3 and IsAltKeyDown()) or
+			(ns.options.HideModifier == 4 and IsControlKeyDown()) or
+			(ns.options.HideModifier == 5 and IsShiftKeyDown()) then
+			return true;
+		else
+			return false;
+		end		
+	end
+
+	return isShow(unit);
+end
+
+local isTank = false;
+
+local function mustShow(unit)
+	if UnitIsUnit(unit, "target") or UnitIsUnit(unit, "focus") then
+		return true;
+	end
+
+	local name, _, _, _, _, _, _, _, spellid = UnitCastingInfo(unit);
+	if not name then
+		name, _, _, _, _, _, _, _, spellid = UnitChannelInfo(unit);
+	end
+
+	local status = UnitThreatSituation("player", unit);
+
+	if ns.options.Show_DBM_Interrupt_Only then
+		-- 차단 가능 스킬 모두 발동
+		if name and spellid and status and DangerousSpellList[spellid] then
+			return true;
+		end
+	else
+		if name and spellid and status then
+			return true;
+		end
+	end
+
+	if isTank and status and status < 2 then
+		return true;
+	end
+
+	return false;
+end
+
+local function hideNameplates(nameplate, bshow)
+	if not nameplate or nameplate:IsForbidden() then
+		return;
+	end
+
+	if not nameplate.UnitFrame or nameplate.UnitFrame:IsForbidden() then
+		return false;
+	end
+
+	local unit = nameplate.namePlateUnitToken;
+
+	if isFaction(unit) == false then
+		return;
+	end
+
+	local unitframe = getUnitFrame(nameplate);
+
+	if bshow then
+		unitframe:Show();
+		return;
+	end
+
+	if mustShow(unit) then
+		unitframe:Show();
+	else
+		unitframe:Hide();
+	end
+end
+
+local needtowork = false;
+
+local function AHNameP_OnUpdate()
+	local needtohide = false;
+
+	if needtowork then
+		for _, v in pairs(C_NamePlate.GetNamePlates(issecure())) do
+			local nameplate = v;
+
+			if (nameplate) then
+				if checkNeedtoHide(nameplate) then
+					needtohide = true;
+					break;
+				end
+			end
+		end
+
+		for _, v in pairs(C_NamePlate.GetNamePlates(issecure())) do
+			local nameplate = v;
+
+			if (nameplate) then
+				hideNameplates(nameplate, (not needtohide));
+			end
+		end
+	end
+end
+
+local function AHNameP_OnEvent(self, event, ...)
+	if event == "PLAYER_ENTERING_WORLD" then
+		needtowork = false;
+		local inInstance, instanceType = IsInInstance();
+
+		if (inInstance and instanceType == "party") then
+			needtowork = true;
+		end
+
+		isTank = false;
+		local assignedRole = UnitGroupRolesAssigned("player");
+
+		if (assignedRole and assignedRole == "TANK") then
+			isTank = true;
+		end
+	end
+end
+
+local DBMobj;
+
+local function scanDBM()
+	DangerousSpellList = {};
+	if DBMobj.Mods then
+		for i, mod in ipairs(DBMobj.Mods) do
+			if mod.announces then
+				for k, obj in pairs(mod.announces) do
+					if obj.spellId and obj.announceType then
+						if obj.announceType == "interrupt" then
+							DangerousSpellList[obj.spellId] = obj.announceType;
+						end
+					end
+				end
+			end
+			if mod.specwarns then
+				for k, obj in pairs(mod.specwarns) do
+					if obj.spellId and obj.announceType then
+						if obj.announceType == "interrupt" then
+							DangerousSpellList[obj.spellId] = obj.announceType;
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+local function NewMod(self, ...)
+	DBMobj = self;
+	C_Timer.After(0.25, scanDBM);
+end
+
+
+local function initAddon()
+	ns.SetupOptionPanels();
+	AHNameP = CreateFrame("Frame", nil, UIParent)
+
+	AHNameP:RegisterEvent("PLAYER_ENTERING_WORLD");
+	AHNameP:RegisterEvent("TRAIT_CONFIG_UPDATED");
+	AHNameP:RegisterEvent("TRAIT_CONFIG_LIST_UPDATED");
+	AHNameP:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED");
+
+	AHNameP:SetScript("OnEvent", AHNameP_OnEvent)
+	--주기적으로 Callback
+	C_Timer.NewTicker(AHNameP_UpdateRate, AHNameP_OnUpdate);
+
+	local bloaded = C_AddOns.LoadAddOn("DBM-Core");
+	if bloaded then
+		hooksecurefunc(DBM, "NewMod", NewMod)
+	end
+end
+
+BINDING_NAME_AHNP_MODIFER_KEY = "asHideNamePlates Key"
+initAddon();
---
+++ asHideNamePlates/asHideNamePlatesOption.lua
@@ -0,0 +1,88 @@
+local _, ns = ...;
+
+local Options_Default = {
+    version = 250319,
+    HideModifier = 6,
+    Check_DBM_Interrupt_Only = true,
+    Show_DBM_Interrupt_Only = true,
+};
+
+ns.options = CopyTable(Options_Default);
+
+local modifier_options = {
+    [1] = "Key Binding",
+    [2] = "ALT + CTRL",
+    [3] = "ALT",
+    [4] = "CTRL",
+    [5] = "SHIFT",
+    [6] = "Automactically work",
+
+}
+
+local tempoption = {};
+
+function ns.SetupOptionPanels()
+    local function OnSettingChanged(_, setting, value)
+        local function get_variable_from_cvar_name(cvar_name)
+            local variable_start_index = string.find(cvar_name, "_") + 1
+            local variable = string.sub(cvar_name, variable_start_index)
+            return variable
+        end
+
+        local cvar_name = setting:GetVariable()
+        local variable = get_variable_from_cvar_name(cvar_name)
+        AHNP_Options[variable] = value;
+        ns.options[variable] = value;
+        ReloadUI();
+    end
+
+    local category = Settings.RegisterVerticalLayoutCategory("asHideNamePlates");
+
+
+
+    if AHNP_Options == nil or AHNP_Options.version ~= Options_Default.version then
+        AHNP_Options = {};
+        AHNP_Options = CopyTable(Options_Default);
+    end
+    ns.options = CopyTable(AHNP_Options);
+
+    for variable, _ in pairs(Options_Default) do
+        if variable ~= "version" then
+            local name = variable;
+            local cvar_name = "asHideNamePlates_" .. variable;
+            local tooltip = ""
+            if AHNP_Options[variable] == nil then
+                AHNP_Options[variable] = Options_Default[variable];
+                ns.options[variable] = Options_Default[variable];
+            end
+            local defaultValue = Options_Default[variable];
+            local currentValue = AHNP_Options[variable];
+
+            if name == "HideModifier" then
+                local function GetOptions()
+                    local container = Settings.CreateControlTextContainer()
+
+                    for id, v in pairs(modifier_options) do
+                        container:Add(id, v);
+                    end
+                    return container:GetData()
+                end
+
+                local setting = Settings.RegisterAddOnSetting(category, cvar_name, variable, tempoption,
+                    type(defaultValue), name, defaultValue);
+
+                Settings.CreateDropdown(category, setting, GetOptions, tooltip);
+                Settings.SetValue(cvar_name, currentValue);
+                Settings.SetOnValueChangedCallback(cvar_name, OnSettingChanged);
+            else
+                local setting = Settings.RegisterAddOnSetting(category, cvar_name, variable, tempoption,
+                    type(defaultValue), name, defaultValue);
+                Settings.CreateCheckboxWithOptions(category, setting, nil, tooltip);
+                Settings.SetValue(cvar_name, currentValue);
+                Settings.SetOnValueChangedCallback(cvar_name, OnSettingChanged);
+            end
+        end
+    end
+
+    Settings.RegisterAddOnCategory(category)
+end
