2f5ab0c3a4d90419e3fce2d31e189d672909ada1
aspilla <aspilla@gmail.com>
2025-07-26 21:37:04 +0900

    250726

--- asRangeDisplay/asRangeDisplay.lua
+++ asRangeDisplay/asRangeDisplay.lua
@@ -1,13 +1,20 @@
+local _, ns           = ...;
 -- 설정 (변경가능)
 local ARD_Font        = STANDARD_TEXT_FONT;
 local ARD_FontSize    = 16;
 local ARD_FontOutline = "THICKOUTLINE";
 local ARD_X           = 0;
 local ARD_Y           = -130;
-local ARD_AHT         = false -- asHealthText에 연결
-local ARD_UpdateRate  = 0.25  -- Update 주기
+local ARD_Focus_X     = 365;
+local ARD_Focus_Y     = -220;
+local ARD_Mouse_X     = 50;
+local ARD_Mouse_Y     = 0;
+local ARD_UpdateRate  = 0.25 -- Update 주기
 -- 설정끝
-local FriendItems     = {
+
+
+
+local FriendItems       = {
 	{ 37727,  5 }, -- Ruby Acorn
 	{ 63427,  6 }, -- Worgsaw
 	{ 34368,  8 }, -- Attuned Crystal Cores
@@ -25,7 +32,7 @@ local FriendItems     = {
 	{ 35278,  80 }, -- Reinforced Net
 }
 
-local HarmItems       = {
+local HarmItems         = {
 	{ 37727,  5 }, -- Ruby Acorn
 	{ 63427,  6 }, -- Worgsaw
 	{ 34368,  8 }, -- Attuned Crystal Cores
@@ -44,89 +51,50 @@ local HarmItems       = {
 	{ 33119,  100 }, -- Malister's Frost Wand
 }
 
-local mspells         = {
-	--악사
-	["혼돈의 일격"] = 3,
-	["영혼 베어내기"] = 3,
-
-	--전사
-	["방패 밀쳐내기"] = 3,
-	["필사의 일격"] = 3,
-	["피의 갈증"] = 3,
-
-	--도적
-	["속결"] = 3,
-	["독살"] = 3,
-	["절개"] = 3,
-
-	--사냥꾼
-	["날개 절단"] = 3,
-
-	--성기사	
-	["정의의 방패"] = 8,
-	["성전사 일격"] = 8,
-	["기사단의 공세"] = 8,
-	["기사단의 베기"] = 8,
-	["성전의 강타"] = 8,
-
-	--죽음의 기사
-	["죽음의 일격"] = 3,
-
-	--수도사
-	["후려차기"] = 3,
-
-	--드루이드
-	["칼날 발톱"] = 9,
-	["짓이기기"] = 11,
-
-	--주술사
-	["폭풍의 일격"] = 3,
-
-
-}
-
-local asGetSpellInfo = function(spellID)
+local asGetSpellInfo    = function(spellID)
 	if not spellID then
 		return nil;
 	end
 
 	local ospellID = C_Spell.GetOverrideSpell(spellID)
 
-    if ospellID then
-        spellID = ospellID;
-    end
+	if ospellID then
+		spellID = ospellID;
+	end
 
 	local spellInfo = C_Spell.GetSpellInfo(spellID);
 	if spellInfo then
-		return spellInfo.name, nil, spellInfo.iconID, spellInfo.castTime, spellInfo.minRange, spellInfo.maxRange, spellInfo.spellID, spellInfo.originalIconID;
+		return spellInfo.name, nil, spellInfo.iconID, spellInfo.castTime, spellInfo.minRange, spellInfo.maxRange,
+			spellInfo.spellID, spellInfo.originalIconID;
 	end
 end
 
 local asGetSpellTabInfo = function(index)
 	local skillLineInfo = C_SpellBook.GetSpellBookSkillLineInfo(index);
 	if skillLineInfo then
-		return	skillLineInfo.name, 
-				skillLineInfo.iconID, 
-				skillLineInfo.itemIndexOffset, 
-				skillLineInfo.numSpellBookItems, 
-				skillLineInfo.isGuild, 
-				skillLineInfo.offSpecID,
-				skillLineInfo.shouldHide,
-				skillLineInfo.specID;
+		return skillLineInfo.name,
+			skillLineInfo.iconID,
+			skillLineInfo.itemIndexOffset,
+			skillLineInfo.numSpellBookItems,
+			skillLineInfo.isGuild,
+			skillLineInfo.offSpecID,
+			skillLineInfo.shouldHide,
+			skillLineInfo.specID;
 	end
 end
 
-local GetItemInfo = C_Item and C_Item.GetItemInfo;
-local IsItemInRange = C_Item and C_Item.IsItemInRange;
+local GetItemInfo       = C_Item and C_Item.GetItemInfo;
+local IsItemInRange     = C_Item and C_Item.IsItemInRange;
 
-local ARD_mainframe   = CreateFrame("Frame", nil, UIParent);
+local ARD_mainframe     = CreateFrame("Frame", nil, UIParent);
 --asOverlay 위에 뜨게 하기 위해 MEDIUM으로 설정
 ARD_mainframe:SetFrameStrata("MEDIUM");
 ARD_mainframe:SetFrameLevel(9000);
-local ARD_RangeText;
+local ARD_TargetRangeText;
+local ARD_MouseRangeText;
+local ARD_FocusRangeText;
 
 local function IsHelpful(unit)
-
 	if UnitIsUnit("player", unit) then
 		return false;
 	else
@@ -138,28 +106,19 @@ local function IsHelpful(unit)
 			return true;
 		end
 	end
-
-	return false;
 end
 
 local function ARD_CheckRange(unit)
-	local isHarm = true;
-	local itemlist = FriendItems;
+	local itemlist = HarmItems;
 
 	if UnitIsUnit("player", unit) then
 		return 0;
-	else
-		local reaction = UnitReaction("player", unit);
-
-		if reaction and reaction <= 4 then
-			isHarm = true;
-		else
-			isHarm = false;
-		end
 	end
 
-	if isHarm then
-		itemlist = HarmItems;
+	local isHelpful = IsHelpful(unit);
+
+	if isHelpful then
+		itemlist = FriendItems;
 	end
 
 	for _, v in pairs(itemlist) do
@@ -189,9 +148,21 @@ local function ARD_GetRangeColor(range)
 end
 
 local cache = {}
+local defaultrange = 40;
 
 local function scanSpells()
+	local localizedClass, englishClass = UnitClass("player");
+
+
+	if englishClass == "EVOKER" then
+		defaultrange = 25;
+	end
+
+
 	cache = {};
+
+
+
 	for tab = 1, 5 do
 		local tabName, tabTexture, tabOffset, numEntries = asGetSpellTabInfo(tab)
 
@@ -200,68 +171,109 @@ local function scanSpells()
 		end
 
 		for i = tabOffset + 1, tabOffset + numEntries do
-			local spellName, _, spellID = C_SpellBook.GetSpellBookItemName(i, Enum.SpellBookSpellBank.Player)
-			local name, rank, icon, castTime, minRange, maxRange, ID, originalIcon = asGetSpellInfo(spellID);
-			local mrange = mspells[name];
+			local spellName = C_SpellBook.GetSpellBookItemName(i, Enum.SpellBookSpellBank.Player);
 
-			if maxRange and (maxRange > 0 or mrange) then
-				if mrange and mrange > maxRange then
-					maxRange = mrange
+			if not spellName then
+				do break end
+			end
+
+			local slotType, actionID, spellID = C_SpellBook.GetSpellBookItemType(i, Enum.SpellBookSpellBank.Player);
+			local isPassive = C_SpellBook.IsSpellBookItemPassive(i, Enum.SpellBookSpellBank.Player)
+			if not isPassive and spellID then
+				local name, rank, icon, castTime, minRange, maxRange, ID, originalIcon = asGetSpellInfo(spellID);
+				if maxRange and maxRange > 0 and maxRange < defaultrange then
+					tinsert(cache, { spellID, maxRange });
 				end
-				tinsert(cache, { i, maxRange });
 			end
 		end
 	end
 end
 
-local function ARD_OnUpdate()
-	if UnitExists("target") then
-		if not InCombatLockdown() or not IsHelpful("target") then
-			local range = ARD_CheckRange("target");
+local function updateUnitRange(unit, frame)
+	if UnitExists(unit) then
+		if not InCombatLockdown() or not IsHelpful(unit) then
+			local range = ARD_CheckRange(unit);
 			if range == 0 then
-				ARD_RangeText:SetText("");
+				frame:SetText("");
 			else
-				ARD_RangeText:SetText(range);
+				frame:SetText(range);
 			end
-			ARD_RangeText:SetTextColor(ARD_GetRangeColor(range));
+			frame:SetTextColor(ARD_GetRangeColor(range));
 		else
-			local grange = 100;
+			local grange = 80;
+			local inRange, checkedRange = UnitInRange(unit)
 
-			for _, v in pairs(cache) do
-				local able = C_Spell.IsSpellInRange(v[1])
+			if inRange and checkedRange then
+				grange = defaultrange;
+			end
 
-				if able == 1 and v[2] < grange then
-					grange = v[2];
+			if UnitIsUnit(unit, "target") then
+				for _, v in pairs(cache) do
+					if v[2] < grange then
+						able = C_Spell.IsSpellInRange(v[1])
+
+						if able then
+							grange = v[2];
+						end
+					end
 				end
 			end
 
-			ARD_RangeText:SetText(math.floor(grange + 0.5));
-			ARD_RangeText:SetTextColor(ARD_GetRangeColor(grange));
+			frame:SetText(math.floor(grange + 0.5));
+			frame:SetTextColor(ARD_GetRangeColor(grange));
 		end
 	else
-		ARD_RangeText:SetText("");
+		frame:SetText("");
 	end
+end
+
+local function ARD_OnUpdateTarget()
+	if ns.options.ShowTarget then
+		updateUnitRange("target", ARD_TargetRangeText);
+	end
+end
+
+local function ARD_OnUpdateMouse()
+	if ns.options.ShowMouseOver then
+		updateUnitRange("mouseover", ARD_MouseRangeText);
+	end
+end
+
+local function ARD_OnUpdateFocus()
+	if ns.options.ShowFocus then
+		updateUnitRange("focus", ARD_FocusRangeText);
+	end
+end
+
+local function ARD_OnMouseUpdate()
+	ARD_MouseRangeText:ClearAllPoints();
+	local uiScale, x, y = UIParent:GetEffectiveScale(), GetCursorPosition()
+	ARD_MouseRangeText:SetPoint("CENTER", UIParent, "BOTTOMLEFT", x / uiScale + ARD_Mouse_X, y / uiScale + ARD_Mouse_Y);
 end
 
 local function ARD_OnLoad()
-	ARD_RangeText = ARD_mainframe:CreateFontString(nil, "OVERLAY")
-	ARD_RangeText:SetFont(ARD_Font, ARD_FontSize, ARD_FontOutline)
+	ARD_TargetRangeText = ARD_mainframe:CreateFontString(nil, "OVERLAY")
+	ARD_TargetRangeText:SetFont(ARD_Font, ARD_FontSize, ARD_FontOutline)
+	ARD_TargetRangeText:SetPoint("CENTER", UIParent, "CENTER", ARD_X, ARD_Y);
+	ARD_TargetRangeText:SetText("");
+	ARD_TargetRangeText:Show();
 
-	local bloaded = C_AddOns.LoadAddOn("asHealthText")
+	ARD_MouseRangeText = ARD_mainframe:CreateFontString(nil, "OVERLAY")
+	ARD_MouseRangeText:SetFont(ARD_Font, ARD_FontSize, ARD_FontOutline)
+	ARD_MouseRangeText:SetPoint("CENTER", UIParent, "CENTER", ARD_X, ARD_Y);
+	ARD_MouseRangeText:SetText("");
+	ARD_MouseRangeText:Show();
 
-	if bloaded and ARD_AHT then
-		ARD_RangeText:SetPoint("LEFT", "AHT_RaidIcon", "RIGHT", 2, 0);
-	else
-		ARD_RangeText:SetPoint("CENTER", UIParent, "CENTER", ARD_X, ARD_Y);
-	end
-
-	ARD_RangeText:SetText("");
-	ARD_RangeText:Show();
-
-	local bloaded = C_AddOns.LoadAddOn("asMOD")
+	ARD_FocusRangeText = ARD_mainframe:CreateFontString(nil, "OVERLAY")
+	ARD_FocusRangeText:SetFont(ARD_Font, ARD_FontSize, ARD_FontOutline)
+	ARD_FocusRangeText:SetPoint("CENTER", UIParent, "CENTER", ARD_Focus_X, ARD_Focus_Y);
+	ARD_FocusRangeText:SetText("");
+	ARD_FocusRangeText:Show();
+	bloaded = C_AddOns.LoadAddOn("asMOD")
 
 	if bloaded and asMOD_setupFrame then
-		asMOD_setupFrame(ARD_RangeText, "asRangeDisplay");
+		asMOD_setupFrame(ARD_TargetRangeText, "asRangeDisplay(Target)");
+		asMOD_setupFrame(ARD_FocusRangeText, "asRangeDisplay(Focus)");
 	end
 
 	ARD_mainframe:RegisterEvent("PLAYER_TARGET_CHANGED");
@@ -272,14 +284,23 @@ local function ARD_OnLoad()
 	ARD_mainframe:RegisterEvent("PLAYER_REGEN_ENABLED");
 end
 
+local bfirst = true;
+
 local function ARD_OnEvent(self, event, ...)
 	if event == "PLAYER_TARGET_CHANGED" then
-		ARD_OnUpdate();
+		ARD_OnUpdateTarget();
 	else
+		if bfirst then
+			bfirst = false;
+			ns.SetupOptionPanels();
+		end
 		scanSpells();
 	end
 end
 
 ARD_mainframe:SetScript("OnEvent", ARD_OnEvent);
 ARD_OnLoad();
-C_Timer.NewTicker(ARD_UpdateRate, ARD_OnUpdate);
+C_Timer.NewTicker(ARD_UpdateRate, ARD_OnUpdateTarget);
+C_Timer.NewTicker(ARD_UpdateRate, ARD_OnUpdateMouse);
+C_Timer.NewTicker(ARD_UpdateRate, ARD_OnUpdateFocus);
+C_Timer.NewTicker(0.05, ARD_OnMouseUpdate);
---
+++ asRangeDisplay/asRangeDisplayOption.lua
@@ -0,0 +1,60 @@
+local _, ns = ...;
+local Options_Default = {
+    version = 250726,
+    ShowTarget = true,
+    ShowMouseOver = true,
+    ShowFocus = true,
+};
+
+ns.options = CopyTable(Options_Default);
+
+local tempoption = {};
+
+function ns.SetupOptionPanels()
+    local function OnSettingChanged(_, setting, value)
+        local function get_variable_from_cvar_name(cvar_name)
+            local variable_start_index = string.find(cvar_name, "_") + 1
+            local variable = string.sub(cvar_name, variable_start_index)
+            return variable
+        end
+
+        local cvar_name = setting:GetVariable()
+        local variable = get_variable_from_cvar_name(cvar_name)
+        ARD_Options[variable] = value;
+        ns.options[variable] = value;
+        ReloadUI();
+    end
+
+    local category = Settings.RegisterVerticalLayoutCategory("asRangeDisplay")
+
+
+
+    if ARD_Options == nil or ARD_Options.version ~= Options_Default.version then
+        ARD_Options = {};
+        ARD_Options = CopyTable(Options_Default);
+    end
+    ns.options = CopyTable(ARD_Options);
+
+    for variable, _ in pairs(Options_Default) do
+        if variable ~= "version" then
+            local name = variable;
+            local cvar_name = "asRangeDisplay_" .. variable;
+            local tooltip = ""
+            if ARD_Options[variable] == nil then
+                ARD_Options[variable] = Options_Default[variable];
+                ns.options[variable] = Options_Default[variable];
+            end
+            local defaultValue = Options_Default[variable];
+            local currentValue = ARD_Options[variable];
+
+            local setting = Settings.RegisterAddOnSetting(category, cvar_name,  variable, tempoption, type(defaultValue), name, defaultValue);
+            Settings.CreateCheckboxWithOptions(category, setting, nil, tooltip);
+            Settings.SetValue(cvar_name, currentValue);
+            Settings.SetOnValueChangedCallback(cvar_name, OnSettingChanged);
+        end
+    end
+
+    Settings.RegisterAddOnCategory(category)
+end
+
+
