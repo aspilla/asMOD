b2a638f5ac6ad36a47299ec1324960e257602597
aspilla <aspilla@gmail.com>
2026-01-09 11:31:09 +0900

    260109

--- asCombatInfo/asCombatInfo.lua
+++ asCombatInfo/asCombatInfo.lua
@@ -226,13 +226,17 @@ local function update_buttons(viewer)
 		end
 
 		if not isbuff then
-			local keytext = ns.hotkeys[button:GetSpellID()];
+			local spellid = button:GetSpellID();
 
-			if keytext then
-				button.hotkey:SetText(keytext);
-				button.hotkey:Show();
-			else
-				button.hotkey:Hide();
+			if not issecretvalue(spellid) then
+				local keytext = ns.hotkeys[button:GetSpellID()];
+
+				if keytext then
+					button.hotkey:SetText(keytext);
+					button.hotkey:Show();
+				else
+					button.hotkey:Hide();
+				end
 			end
 		end
 	end
@@ -325,45 +329,45 @@ local viewers = {
 }
 
 local function check_name(name)
-  name = string.gsub(name, "Num Pad ", "");
-  name = string.gsub(name, "숫자패드 ", "");
-  name = string.gsub(name, "Num Pad", "");
-  name = string.gsub(name, "숫자패드", "");
+	name = string.gsub(name, "Num Pad ", "");
+	name = string.gsub(name, "숫자패드 ", "");
+	name = string.gsub(name, "Num Pad", "");
+	name = string.gsub(name, "숫자패드", "");
 
-  name = string.gsub(name, "Middle Mouse", "M3");
-  name = string.gsub(name, "마우스 가운데 버튼", "M3");
-  name = string.gsub(name, "Mouse Button (%d)", "M%1");
-  name = string.gsub(name, "(%d)번 마우스 버튼", "M%1");
-  name = string.gsub(name, "Mouse Wheel Up", "MU");
-  name = string.gsub(name, "마우스 휠 위로", "MU");
-  name = string.gsub(name, "Mouse Wheel Down", "MD");
-  name = string.gsub(name, "마우스 휠 아래로", "MD");
-  name = string.gsub(name, "^s%-", "S");
-  name = string.gsub(name, "^a%-", "A");
-  name = string.gsub(name, "^c%-", "C");
-  name = string.gsub(name, "Delete", "Dt");
-  name = string.gsub(name, "Page Down", "Pd");
-  name = string.gsub(name, "Page Up", "Pu");
-  name = string.gsub(name, "Insert", "In");
-  name = string.gsub(name, "Del", "Dt");
-  name = string.gsub(name, "Home", "Hm");
-  name = string.gsub(name, "Capslock", "Ck");
-  name = string.gsub(name, "Num Lock", "Nk");
-  name = string.gsub(name, "Scroll Lock", "Sk");
-  name = string.gsub(name, "Backspace", "Bs");
-  name = string.gsub(name, "Spacebar", "Sb");
-  name = string.gsub(name, "스페이스 바", "Sb");
-  name = string.gsub(name, "End", "Ed");
-  name = string.gsub(name, "Up Arrow", "^");
-  name = string.gsub(name, "위 화살표", "^");
-  name = string.gsub(name, "Down Arrow", "V");
-  name = string.gsub(name, "아래 화살표", "V");
-  name = string.gsub(name, "Right Arrow", ">");
-  name = string.gsub(name, "오른쪽 화살표", ">");
-  name = string.gsub(name, "Left Arrow", "<");
-  name = string.gsub(name, "왼쪽 화살표", "<");
+	name = string.gsub(name, "Middle Mouse", "M3");
+	name = string.gsub(name, "마우스 가운데 버튼", "M3");
+	name = string.gsub(name, "Mouse Button (%d)", "M%1");
+	name = string.gsub(name, "(%d)번 마우스 버튼", "M%1");
+	name = string.gsub(name, "Mouse Wheel Up", "MU");
+	name = string.gsub(name, "마우스 휠 위로", "MU");
+	name = string.gsub(name, "Mouse Wheel Down", "MD");
+	name = string.gsub(name, "마우스 휠 아래로", "MD");
+	name = string.gsub(name, "^s%-", "S");
+	name = string.gsub(name, "^a%-", "A");
+	name = string.gsub(name, "^c%-", "C");
+	name = string.gsub(name, "Delete", "Dt");
+	name = string.gsub(name, "Page Down", "Pd");
+	name = string.gsub(name, "Page Up", "Pu");
+	name = string.gsub(name, "Insert", "In");
+	name = string.gsub(name, "Del", "Dt");
+	name = string.gsub(name, "Home", "Hm");
+	name = string.gsub(name, "Capslock", "Ck");
+	name = string.gsub(name, "Num Lock", "Nk");
+	name = string.gsub(name, "Scroll Lock", "Sk");
+	name = string.gsub(name, "Backspace", "Bs");
+	name = string.gsub(name, "Spacebar", "Sb");
+	name = string.gsub(name, "스페이스 바", "Sb");
+	name = string.gsub(name, "End", "Ed");
+	name = string.gsub(name, "Up Arrow", "^");
+	name = string.gsub(name, "위 화살표", "^");
+	name = string.gsub(name, "Down Arrow", "V");
+	name = string.gsub(name, "아래 화살표", "V");
+	name = string.gsub(name, "Right Arrow", ">");
+	name = string.gsub(name, "오른쪽 화살표", ">");
+	name = string.gsub(name, "Left Arrow", "<");
+	name = string.gsub(name, "왼쪽 화살표", "<");
 
-  return name;
+	return name;
 end
 
 local function scan_keys(name, type, hide, total)
