2117cb4b7d6451837d91798d80e114ce7e85ca60
aspilla <aspilla@gmail.com>
2026-01-16 20:56:02 +0900

    260116

--- asGearScoreLite/asGearScoreLite.lua
+++ asGearScoreLite/asGearScoreLite.lua
@@ -1,5 +1,6 @@
 local configs = {
 	fontsize = 11,
+	font = STANDARD_TEXT_FONT,
 };
 
 local itemslots = {
@@ -22,74 +23,126 @@ local itemslots = {
 };
 
 local main_frame = CreateFrame("Frame", nil, UIParent);
-local fontstrings = {};
+local leveltexts = {};
 local inspectframe = _G["InspectFrame"];
 local avg_text;
 local bfirst = true;
 
-local function get_avglevel(unit)
-	local t, c = 0, 0
+
+local function update_player()
+	local unit = "player";
+
+	for i = 1, #itemslots do
+		local slot = GetInventorySlotInfo(itemslots[i]);		
+		local leveltext = leveltexts[unit][i];
+		local location = ItemLocation:CreateFromEquipmentSlot(slot)
+
+		if location:IsValid() then
+			local item = Item:CreateFromEquipmentSlot(slot);
+			
+			if item then
+				item:ContinueOnItemLoad(function()
+					local level = item:GetCurrentItemLevel();
+					local quality = item:GetItemQuality();
+
+					if level and level > 0 and quality then
+						leveltext:SetText(level);
+						local r, g, b = C_Item.GetItemQualityColor(quality);
+						leveltext:SetTextColor(r, g, b);
+					end
+				end)
+			else
+				leveltext:SetText("");
+			end
+		else
+			leveltext:SetText("");
+		end
+	end
+
+	return 0;
+end
+
+local function get_levelfromslot(unit, slotID)
+    
+    local tooltip = C_TooltipInfo.GetInventoryItem(unit, slotID)
+    
+    if tooltip and tooltip.lines then        
+        for _, line in ipairs(tooltip.lines) do
+            if line.leftText then                
+                local iLevel = line.leftText:match("%d+")
+                if line.leftText:find(STAT_AVERAGE_ITEM_LEVEL) then
+                    return tonumber(iLevel)
+                end
+            end
+        end
+    end
+    return nil
+end
+
+local function update_target()
+	local total_lvl, count = 0, 0
 	local weapon_lvl;
 	local weapon_count = 0;
 	local two_head = nil;
+	local unit = "target";
 
 	for i = 1, #itemslots do
-		local idx = GetInventorySlotInfo(itemslots[i]);
-		local k = GetInventoryItemLink(unit, idx);
+		local slot = GetInventorySlotInfo(itemslots[i]);
+		local link = GetInventoryItemLink(unit, slot);
+		local leveltext = leveltexts[unit][i];
 
-		if k then
-			local _, _, quality = C_Item.GetItemInfo(k);
-			local lvl = C_Item.GetDetailedItemLevelInfo(k);
+		if link then
+			local _, _, quality = C_Item.GetItemInfo(link);
+			--local level = C_Item.GetDetailedItemLevelInfo(link);
+			local level = get_levelfromslot(unit, slot)
 
-			if lvl and lvl > 0 and quality then
-				fontstrings[unit][i]:SetText(lvl);
+			if level and level > 0 and quality then
+				leveltext:SetText(level);
 				local r, g, b = C_Item.GetItemQualityColor(quality);
-				fontstrings[unit][i]:SetTextColor(r, g, b);
+				leveltext:SetTextColor(r, g, b);
 			end
 
-			if idx == 16 or idx == 17 then
+			if slot == 16 or slot == 17 then
 				weapon_count = weapon_count + 1;
-				two_head = lvl;
+				two_head = level;
 			end
 
-			if lvl and lvl > 0 then
-				t = t + lvl
-				c = c + 1
+			if level and level > 0 then
+				total_lvl = total_lvl + level
+				count = count + 1
 			end
 		else
-			fontstrings[unit][i]:SetText("");
+			leveltext:SetText("");
 		end
 	end
 
 	if weapon_lvl then
-		t = t + (weapon_lvl * 2)
-		c = c + 2
+		total_lvl = total_lvl + (weapon_lvl * 2)
+		count = count + 2
 	end
 
 	if weapon_count == 1 and two_head then
-		t = t + two_head;
-		c = c + 1;
+		total_lvl = total_lvl + two_head;
+		count = count + 1;
 	end
 
-	if c > 0 then
-		return floor(t / c);
+	if count > 0 then
+		avg_text:SetText(floor(total_lvl / count) .. " Lvl");
 	else
-		return 0;
+		avg_text:SetText("");
 	end
 end
 
 local function check_player()
 	if (InCombatLockdown()) then return; end
-
-	local Avg = get_avglevel("player");
+	update_player();
 end
 
 local function check_inspect()
 	if (InCombatLockdown()) then return; end
 
 	if CanInspect("target") then
-		local Avg = get_avglevel("target");
-		avg_text:SetText(Avg .. " Lvl");
+		update_target();
 	end
 end
 
@@ -116,32 +169,32 @@ main_frame:RegisterEvent("ADDON_LOADED");
 CharacterFrame:HookScript("OnShow", check_player);
 
 avg_text = inspectframe:CreateFontString(nil, "OVERLAY");
-avg_text:SetFont(STANDARD_TEXT_FONT, configs.fontsize, "THICKOUTLINE");
+avg_text:SetFont(configs.font, configs.fontsize, "THICKOUTLINE");
 avg_text:SetText("Avg: 0")
 avg_text:SetPoint("TOPRIGHT", inspectframe, "TOPRIGHT", -10, -30)
 avg_text:SetTextColor(1, 1, 1);
 avg_text:Show()
 
-fontstrings["player"] = {};
-fontstrings["target"] = {};
+leveltexts["player"] = {};
+leveltexts["target"] = {};
 
 for slot, n in pairs(itemslots) do
 	local button = _G["Character" .. n]
-	local lvltext = main_frame:CreateFontString(nil, "OVERLAY");
-	lvltext:SetParent(button);
-	lvltext:SetFont(STANDARD_TEXT_FONT, configs.fontsize, "THICKOUTLINE");
-	lvltext:SetPoint("TOP", button, "TOP", 0, -3);
-	lvltext:SetTextColor(1, 1, 1);
-	fontstrings["player"][slot] = lvltext;
+	local leveltext = main_frame:CreateFontString(nil, "OVERLAY");
+	leveltext:SetParent(button);
+	leveltext:SetFont(configs.font, configs.fontsize, "THICKOUTLINE");
+	leveltext:SetPoint("TOP", button, "TOP", 0, -3);
+	leveltext:SetTextColor(1, 1, 1);
+	leveltexts["player"][slot] = leveltext;
 end
 
 
 for slot, n in pairs(itemslots) do
 	local button = _G["Inspect" .. n]
-	local lvltext = main_frame:CreateFontString(nil, "OVERLAY");
-	lvltext:SetParent(button);
-	lvltext:SetFont(STANDARD_TEXT_FONT, configs.fontsize, "THICKOUTLINE");
-	lvltext:SetPoint("TOP", button, "TOP", 0, -3);
-	lvltext:SetTextColor(1, 1, 1);
-	fontstrings["target"][slot] = lvltext;
+	local leveltext = main_frame:CreateFontString(nil, "OVERLAY");
+	leveltext:SetParent(button);
+	leveltext:SetFont(configs.font, configs.fontsize, "THICKOUTLINE");
+	leveltext:SetPoint("TOP", button, "TOP", 0, -3);
+	leveltext:SetTextColor(1, 1, 1);
+	leveltexts["target"][slot] = leveltext;
 end
