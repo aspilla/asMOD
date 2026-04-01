928260e3f535d42d3cb98b0d15cac04891a6caf1
aspilla <aspilla@gmail.com>
2026-04-01 18:22:39 +0900

    asBuffFilter260401

--- asBuffFilter/asBuffFilter.lua
+++ asBuffFilter/asBuffFilter.lua
@@ -6,7 +6,7 @@ local function clear_cooldownframe(self)
 end
 
 local function set_cooldownframe(self, durationobject, enable)
-    if enable then
+    if enable and durationobject then
         self:SetDrawEdge(nil);
         self:SetCooldownFromDurationObject(durationobject);
     else
