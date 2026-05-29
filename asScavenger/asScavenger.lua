4c06519724a046192a034b23f0e27a51dfb59ea1
aspilla <aspilla@gmail.com>
2026-05-29 20:00:12 +0900

    asScavenger260530

--- asScavenger/asScavenger.lua
+++ asScavenger/asScavenger.lua
@@ -1,6 +1,15 @@
+local configs = {
+	pertick = 10,
+	ticktime = 1,
+}
 
-local function on_event(self)
+local gvalues = {
+	timer = nil,
+}
+
+local function get_trashlist()
 	local c = C_Container;
+	local list = {};
 
 	for bag = 0, 4 do
 		if c.GetContainerNumSlots(bag) > 0 then
@@ -11,11 +20,47 @@ local function on_event(self)
 					local itemName, itemLink, itemRarity = C_Item.GetItemInfo(link)
 
 					if itemRarity == 0 then
-						c.UseContainerItem(bag, slot)
+						table.insert(list, { b = bag, s = slot });
 					end
 				end
 			end
 		end
+	end
+
+	return list;
+end
+
+
+
+
+
+local function on_event(self)
+	local list = get_trashlist();
+	local count = math.floor(#list/configs.pertick) + 1;
+	local c = C_Container;
+	local idx = 1;
+
+	local function sell_trash()
+		for i = 1, configs.pertick do
+			if idx > #list then
+				gvalues.timer:Cancel();
+				return;
+			end
+
+			local bag = list[idx].b;
+			local slot = list[idx].s;
+
+			c.UseContainerItem(bag, slot);
+			idx = idx + 1;
+		end
+	end
+
+	if gvalues.timer then
+		gvalues.timer:Cancel();
+	end
+
+	if count > 0 then
+		gvalues.timer = C_Timer.NewTicker(configs.ticktime, sell_trash, count);
 	end
 end
 
