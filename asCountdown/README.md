b2deedb3e9e0f0ed5a76dd6ce5bfb6c705e23271
aspilla <aspilla@gmail.com>
2026-06-06 13:42:25 +0900

    asCountdown260606

---
+++ asCountdown/README.md
@@ -0,0 +1,31 @@
+# asCountdown (Midnight)
+
+Countdown Voice Alerts
+
+## Key Features
+* **Countdown Voice Alerts**: Plays voice announcements for 5, 4, 3, 2, 1 seconds only.
+    
+## Configuration
+* No configuration options available.
+* Voice files can be customized by replacing the `mp3` files inside the `sounds` folder (English) or `sounds_kr` folder (Korean).
+
+## Contact
+1. **Korean Users**: Visit the [Inven asMOD Forum](https://www.inven.co.kr/board/wow/5288)
+2. **English Users**: Visit the [asMOD YouTube Channel](https://www.youtube.com/@asmod-wow) or [GitHub](https://github.com/aspilla/asMOD/)
+
+---
+
+# asCountdown (한밤)
+
+초읽기 음성 알림
+
+## 주요 기능
+*  **초읽이 음성 알림**: 5초 4초 3초 2초 1초만 음성으로 알림    
+    
+## 설정
+*  설정 없음
+*  `sounds` 폴더 (영문), `sounds_kr` 폴더 (한글) 내 `mp3` 파일 변경으로 음성 변경 가능
+
+## 문의처
+1. `한글 유저` : `인벤 asMOD 포럼` 방문 (https://www.inven.co.kr/board/wow/5288)
+2. `영문 유저` : `Youtube asMOD 채널` 방문 (https://www.youtube.com/@asmod-wow), `Github` 방문 (https://github.com/aspilla/asMOD/)
---
+++ asCountdown/asCountdown.lua
@@ -0,0 +1,67 @@
+﻿local configs = {
+	sounds = {
+		[5] = "Interface\\AddOns\\asCountdown\\sounds\\5.mp3",
+		[4] = "Interface\\AddOns\\asCountdown\\sounds\\4.mp3",
+		[3] = "Interface\\AddOns\\asCountdown\\sounds\\3.mp3",
+		[2] = "Interface\\AddOns\\asCountdown\\sounds\\2.mp3",
+		[1] = "Interface\\AddOns\\asCountdown\\sounds\\1.mp3",
+	}
+}
+
+if GetLocale() == "koKR" then
+	configs.sounds = {
+		[5] = "Interface\\AddOns\\asCountdown\\sounds_kr\\5.mp3",
+		[4] = "Interface\\AddOns\\asCountdown\\sounds_kr\\4.mp3",
+		[3] = "Interface\\AddOns\\asCountdown\\sounds_kr\\3.mp3",
+		[2] = "Interface\\AddOns\\asCountdown\\sounds_kr\\2.mp3",
+		[1] = "Interface\\AddOns\\asCountdown\\sounds_kr\\1.mp3",
+	}
+end
+
+local gvalues = {
+	endtime = nil,
+	timers = {},
+}
+
+local main_frame = CreateFrame("Frame", nil, UIParent);
+
+local function sound(sec)
+	PlaySoundFile(configs.sounds[sec], "Master");
+end
+
+local function cancel_timers()
+	for i = 1, 5 do
+		local timer = gvalues.timers[i];
+		if timer then
+			timer:Cancel();
+		end
+	end
+end
+
+local function set_timers(total)
+	cancel_timers();
+	for i = 1, 5 do
+		if total >= i then
+			local func = function()
+				sound(i);
+			end
+			gvalues.timers[i] = C_Timer.NewTicker(total - i, func, 1);
+		end
+	end
+
+end
+
+
+
+local function on_event(self, event, ...)
+	if event == "START_PLAYER_COUNTDOWN" then
+		local _, remain, total = ...;		
+		set_timers(total);
+	elseif event == "CANCEL_PLAYER_COUNTDOWN" then		
+		cancel_timers();
+	end
+end
+
+main_frame:RegisterEvent("START_PLAYER_COUNTDOWN");
+main_frame:RegisterEvent("CANCEL_PLAYER_COUNTDOWN");
+main_frame:SetScript("OnEvent", on_event);
